f25e17025b2ff581f83fd164cd2016b3ce3247eb
KoH <cmgaudet2010@gmail.com>
2024-06-06 21:39:29 +0700

    Clownacy Mega-Fix Pt 6: Obj4E

--- _incObj/4E Wall of Lava.asm
+++ _incObj/4E Wall of Lava.asm
@@ -6,9 +6,10 @@ LavaWall:
 		moveq	#0,d0
 		move.b	obRoutine(a0),d0
 		move.w	LWall_Index(pc,d0.w),d1
-		jmp	LWall_Index(pc,d1.w)
+		jmp		LWall_Index(pc,d1.w)
 ; ===========================================================================
-LWall_Index:	dc.w LWall_Main-LWall_Index
+LWall_Index:
+		dc.w LWall_Main-LWall_Index
 		dc.w LWall_Solid-LWall_Index
 		dc.w LWall_Action-LWall_Index
 		dc.w LWall_Move-LWall_Index
@@ -42,7 +43,7 @@ LWall_Main:	; Routine 0
 		move.l	a0,objoff_3C(a1)
 
 .fail:
-		dbf	d1,.loop	; repeat sequence once
+		dbf		d1,.loop	; repeat sequence once
 
 		addq.b	#6,obRoutine(a1)
 		move.b	#4,obFrame(a1)
@@ -69,9 +70,9 @@ LWall_Action:	; Routine 4
 ; ===========================================================================
 
 .movewall:
-		tst.b	lwall_flag(a0)	; is object set	to move?
-		beq.s	LWall_Solid	; if not, branch
-		move.w	#$180,obVelX(a0) ; set object speed
+		tst.b	lwall_flag(a0)		; is object set	to move?
+		beq.s	LWall_Solid			; if not, branch
+		move.w	#$180,obVelX(a0)	; set object speed
 		subq.b	#2,obRoutine(a0)
 
 LWall_Solid:	; Routine 2
@@ -85,30 +86,29 @@ LWall_Solid:	; Routine 2
 		bsr.w	SolidObject
 		move.w	(sp)+,d0
 		move.b	d0,obRoutine(a0)
-		cmpi.w	#$6A0,obX(a0)	; has object reached $6A0 on the x-axis?
-		bne.s	.animate	; if not, branch
-		clr.w	obVelX(a0)	; stop object moving
+		cmpi.w	#$6A0,obX(a0)		; has object reached $6A0 on the x-axis?
+		bne.s	.animate			; if not, branch
+		clr.w	obVelX(a0)			; stop object moving
 		clr.b	lwall_flag(a0)
 
 .animate:
-		lea	(Ani_LWall).l,a1
+		lea		(Ani_LWall).l,a1
 		bsr.w	AnimateSprite
 		cmpi.b	#4,(v_player+obRoutine).w
 		bhs.s	.rangechk
 		bsr.w	SpeedToPos
 
 .rangechk:
-		bsr.w	DisplaySprite
-		tst.b	lwall_flag(a0)	; is wall already moving?
-		bne.s	.moving		; if yes, branch
+		tst.b	lwall_flag(a0)		; is wall already moving?
+		bne.s	.moving				; if yes, branch
 		out_of_range.s	.chkgone
 
 .moving:
-		rts	
+		bra.w	DisplaySprite	; Clownacy DisplaySprites fix
 ; ===========================================================================
 
 .chkgone:
-		lea	(v_objstate).w,a2
+		lea		(v_objstate).w,a2
 		moveq	#0,d0
 		move.b	obRespawnNo(a0),d0
 		bclr	#7,2(a2,d0.w)
