7dae4c0e848dd2873a3c045a373169ab2f9ab619
KoH <cmgaudet2010@gmail.com>
2024-08-13 00:22:36 +0700

    Simplfy Scrap Brain Zone palette cycle data (Clownacy)

--- _inc/SBZ Palette Scripts.asm
+++ _inc/SBZ Palette Scripts.asm
@@ -2,35 +2,38 @@
 ; Scrap Brain Zone palette cycling script
 ; ---------------------------------------------------------------------------
 
-mSBZp:	macro duration,colours,paladdress,ramaddress
+mSBZh:	macro {INTLABEL},{GLOBALSYMBOLS}
+__LABEL__:
+	dc.w ((__LABEL___end-__LABEL__-2)/6)-1
+	endm
+
+mSBZp:	macro duration,colours,sourceAddress,destinationPaletteIndex
 	dc.b duration, colours
-	dc.w paladdress, ramaddress
+	dc.w sourceAddress, v_palette+destinationPaletteIndex*2
 	endm
 
 ; duration in frames, number of colours, palette address, RAM address
 
-Pal_SBZCycList1:
-	dc.w	((end_SBZCycList1-Pal_SBZCycList1-2)/6)-1
-	mSBZp	7,8,Pal_SBZCyc1,v_palette+$50
-	mSBZp	$D,8,Pal_SBZCyc2,v_palette+$52
-	mSBZp	$E,8,Pal_SBZCyc3,v_palette+$6E
-	mSBZp	$B,8,Pal_SBZCyc5,v_palette+$70
-	mSBZp	7,8,Pal_SBZCyc6,v_palette+$72
-	mSBZp	$1C,$10,Pal_SBZCyc7,v_palette+$7E
-	mSBZp	3,3,Pal_SBZCyc8,v_palette+$78
-	mSBZp	3,3,Pal_SBZCyc8+2,v_palette+$7A
-	mSBZp	3,3,Pal_SBZCyc8+4,v_palette+$7C
-end_SBZCycList1:
+Pal_SBZCycList1: mSBZh
+	mSBZp	 7, 8,Pal_SBZCyc1,$28
+	mSBZp	13, 8,Pal_SBZCyc2,$29
+	mSBZp	14, 8,Pal_SBZCyc3,$37
+	mSBZp	11, 8,Pal_SBZCyc5,$38
+	mSBZp	 7, 8,Pal_SBZCyc6,$39
+	mSBZp	28,16,Pal_SBZCyc7,$3F
+	mSBZp	 3, 3,Pal_SBZCyc8,$3C
+	mSBZp	 3, 3,Pal_SBZCyc8+2,$3D
+	mSBZp	 3, 3,Pal_SBZCyc8+4,$3E
+Pal_SBZCycList1_end:
 	even
 
-Pal_SBZCycList2:
-	dc.w	((end_SBZCycList2-Pal_SBZCycList2-2)/6)-1
-	mSBZp	7,8,Pal_SBZCyc1,v_palette+$50
-	mSBZp	$D,8,Pal_SBZCyc2,v_palette+$52
-	mSBZp	9,8,Pal_SBZCyc9,v_palette+$70
-	mSBZp	7,8,Pal_SBZCyc6,v_palette+$72
-	mSBZp	3,3,Pal_SBZCyc8,v_palette+$78
-	mSBZp	3,3,Pal_SBZCyc8+2,v_palette+$7A
-	mSBZp	3,3,Pal_SBZCyc8+4,v_palette+$7C
-end_SBZCycList2:
+Pal_SBZCycList2: mSBZh
+	mSBZp	 7, 8,Pal_SBZCyc1,$28
+	mSBZp	13, 8,Pal_SBZCyc2,$29
+	mSBZp	 9, 8,Pal_SBZCyc9,$38
+	mSBZp	 7, 8,Pal_SBZCyc6,$39
+	mSBZp	 3, 3,Pal_SBZCyc8,$3C
+	mSBZp	 3, 3,Pal_SBZCyc8+2,$3D
+	mSBZp	 3, 3,Pal_SBZCyc8+4,$3E
+Pal_SBZCycList2_end:
 	even
