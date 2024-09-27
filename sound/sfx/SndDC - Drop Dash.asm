3d54426744e7ad2772f7231a2bfdce236cfca9eb
KoH <cmgaudet2010@gmail.com>
2024-09-27 22:45:18 +0700

    New Drop Dash SFX (DarkShamilKhan)

--- sound/sfx/SndDC - Drop Dash.asm
+++ sound/sfx/SndDC - Drop Dash.asm
@@ -1,34 +1,74 @@
-Sound_DC_Header:
-	smpsHeaderStartSong 3
-	smpsHeaderVoice     Sound_DC_Voices
-	smpsHeaderTempoSFX  $01
-	smpsHeaderChanSFX   $01
+SndXX_DropDash_Header:
+	smpsHeaderStartSong 2, 1
+	smpsHeaderVoice SndXX_DropDash_Voices
+	smpsHeaderTempoSFX	$01
+	smpsHeaderChanSFX 	$01
+		
+	smpsHeaderSFXChannel cFM5, SoundXX_FM5, $00, $05
+		
+; FM4 Data
+SoundXX_FM5:
+	smpsSetvoice $00
+;	smpsAlterVol        $5
+	smpsAlterNote       $D4
+	dc.b	nBb5, $01, smpsNoAttack
+	smpsAlterNote       $00
+	dc.b	$01, smpsNoAttack, nB5, smpsNoAttack, nC6, smpsNoAttack, $01, smpsNoAttack, nCs6, smpsNoAttack
+	smpsAlterNote       $1B
+	dc.b	$01, smpsNoAttack
+	smpsAlterNote       $00
+	dc.b	nD6, smpsNoAttack, nEb6, smpsNoAttack
+	smpsAlterNote       $1E
+	dc.b	$01, smpsNoAttack
+	smpsAlterNote       $00
+	dc.b	nE6, smpsNoAttack, nF6, smpsNoAttack
+	smpsAlterNote       $21
+	dc.b	$01, smpsNoAttack
+	smpsAlterNote       $00
+	dc.b	nFs6, smpsNoAttack, nG6, smpsNoAttack
+	smpsAlterNote       $25
+	dc.b	$01, smpsNoAttack
+	smpsAlterNote       $00
+	dc.b	nAb6, smpsNoAttack, nA6, $07
 
-	smpsHeaderSFXChannel cFM5, Sound_DC_FM5,	$00, $00
+Drop_Loop01:
+	smpsAlterVol        $02
 
-; FM5 Data
-Sound_DC_FM5:
-	smpsSetvoice        $00
-	smpsModSet          $01, $01, $18, $00
-	dc.b	nF2, $30
+Drop_Loop00:
+	dc.b	smpsNoAttack, $01
+	smpsAlterVol        $01
+	smpsLoop            $00, $03, Drop_Loop00
+	dc.b	smpsNoAttack, nA6
+	smpsLoop            $01, $0C, Drop_Loop01
+	smpsAlterVol        $02
+
+Drop_Loop02:
+	dc.b	nRst
+	smpsAlterVol        $01
+	dc.b	smpsNoAttack
+	smpsLoop            $00, $03, Drop_Loop02
+	dc.b	nRst
+	smpsAlterVol        $02
+	dc.b	smpsNoAttack, nRst
+	smpsAlterVol        $01
+	dc.b	smpsNoAttack, nRst
 	smpsStop
-
-Sound_DC_Voices:
+		
+SndXX_DropDash_Voices:
 ;	Voice $00
-;	$03
-;	$0A, $0A, $0F, $0F, 	$1F, $1F, $1F, $1F, 	$00, $00, $0C, $00
-;	$00, $00, $00, $09, 	$FF, $FF, $FF, $0F, 	$21, $10, $10, $80
-	smpsVcAlgorithm     $03
-	smpsVcFeedback      $00
+;	$3C
+;	$00, $44, $02, $02, 	$1F, $1F, $1F, $15, 	$00, $1F, $00, $00
+;	$00, $00, $00, $00, 	$0F, $0F, $0F, $0F, 	$0D, $00, $28, $00
+	smpsVcAlgorithm     $04
+	smpsVcFeedback      $07
 	smpsVcUnusedBits    $00
-	smpsVcDetune        $00, $00, $00, $00
-	smpsVcCoarseFreq    $0F, $0F, $0A, $0A
+	smpsVcDetune        $00, $00, $04, $00
+	smpsVcCoarseFreq    $02, $02, $04, $00
 	smpsVcRateScale     $00, $00, $00, $00
-	smpsVcAttackRate    $1F, $1F, $1F, $1F
+	smpsVcAttackRate    $15, $1F, $1F, $1F
 	smpsVcAmpMod        $00, $00, $00, $00
-	smpsVcDecayRate1    $00, $0C, $00, $00
-	smpsVcDecayRate2    $09, $00, $00, $00
-	smpsVcDecayLevel    $00, $0F, $0F, $0F
+	smpsVcDecayRate1    $00, $00, $1F, $00
+	smpsVcDecayRate2    $00, $00, $00, $00
+	smpsVcDecayLevel    $00, $00, $00, $00
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
-	smpsVcTotalLevel    $00, $10, $10, $21
-
+	smpsVcTotalLevel    $00, $28, $00, $0D		
