c00927da2231cfbfb0fed5a2d1b3ae6b8e49422d
KoH <cmgaudet2010@gmail.com>
2024-11-24 18:24:37 +0700

    Almost finished with the Helix
    Only missing collision

--- _incObj/17 Spiked Pole Helix.asm
+++ _incObj/17 Spiked Pole Helix.asm
@@ -4,60 +4,82 @@
 ; ---------------------------------------------------------------------------
 
 Helix:
+		btst	#6,obRender(a0)		; Is this object set to render sub sprites?
+		bne.s	.SubSprs			; If so, branch
 		moveq	#0,d0
 		move.b	mainspr_routine(a0),d0
 		move.w	Hel_Index(pc,d0.w),d1
 		jmp		Hel_Index(pc,d1.w)
 ; ===========================================================================
+.SubSprs:
+	; child sprite objects only need to be drawn
+		bsr.w	Hel_RotateSpikes
+		move.w	#priority3,d0			; RetroKoH/Devon S3K+ Priority Manager
+		bra.w	DisplaySprite2			; Display sprites
+; ===========================================================================
 Hel_Index:		offsetTable
 		offsetTableEntry.w Hel_Main
 		offsetTableEntry.w Hel_Action
-		offsetTableEntry.w Hel_Action
-		offsetTableEntry.w Hel_Delete
 		offsetTableEntry.w Hel_Display
 
-hel_frame = objoff_3E		; start frame (different for each spike)
-
-;		$29-38 are used for child object addresses
+obHelChild		= objoff_30	; pointer to the helix subsprite object
 ; ===========================================================================
 
 Hel_Main:	; Routine 0
 		addq.b	#2,mainspr_routine(a0)
 		move.l	#Map_Hel,obMap(a0)
 		move.w	#make_art_tile(ArtTile_GHZ_Spike_Pole,2,0),obGfx(a0)
-		move.b	#128,obActWid(a0)
-		move.b	#7,obStatus(a0)				; bit 2 is also set... is this used?
+		move.b	#$80,obActWid(a0)
 		move.b	#4,obRender(a0)
 		move.w	#priority3,obPriority(a0)	; RetroKoH/Devon S3K+ Priority Manager
-		bset	#6,obRender(a0)				; set subsprites flag
+		
+;Hel_MakeSubsprite:
+		bsr.w	FindFreeObj
+		bne.w	.done
+		move.b	obID(a0),obID(a1)			; load obj17
+		move.w	obX(a0),obX(a1)
+		move.w	obY(a0),obY(a1)
+		move.l	obMap(a0),obMap(a1)
+		move.w	obGfx(a0),obGfx(a1)
+		move.b	obRender(a0),obRender(a1)
+		bset	#6,obRender(a1)				; set subsprites flag
+		move.b	#128,mainspr_width(a1)		; base this on subtype later
 
-		; create sub objects
+		; create subsprites
 		moveq	#0,d0
-		move.b	obActWid(a0),d0
-		move.w	obX(a0),d1
+		move.b	mainspr_width(a1),d0
+		move.w	obX(a1),d1
 		sub.w	d0,d1						; move spikes back
 		moveq	#16,d2						; +16 pixels
 		
 		; load 8 spike pole spikes (Change to accomodate subtypes, like the bridge)
-		move.b	#8,mainspr_childsprites(a0)
-		lea		sub2_x_pos(a0),a1
-		lea		obY(a0),a2
+	;	move.b	obSubtype(a0),d1
+	;	andi.b	#7,d1
+	;	addq.b	#1,d1
+	;	move.b	d1,mainspr_childsprites(a1)
+		move.b	#8,mainspr_childsprites(a1)
+		lea		sub2_x_pos(a1),a2			; starting address for subsprite data
+		move.w	obY(a1),d3
 
 	; Perform a loop here once subtype is implemented
 	rept 7
-		move.w	d1,(a1)+					; set xpos
-		move.w	(a2),(a1)					; set ypos
-		addq.w	#4,a1						; skip frame
+		move.w	d1,(a2)+					; set xpos
+		move.w	d3,(a2)						; set ypos
+		addq.w	#4,a2						; skip frame
 		add.w	d2,d1						; +16 pixels
 	endr
 
 		; last spike
-		move.w	d1,(a1)+					; set xpos
-		move.w	(a2),(a1)					; set ypos
+		move.w	d1,(a2)+					; set xpos
+		move.w	d3,(a2)						; set ypos
 
-Hel_Action:	; Routine 2, 4
-		bsr.w	Hel_RotateSpikes
-		bra.w	Hel_ChkDel			; Clownacy DisplaySprite Fix
+.done:
+		move.l	a1,obHelChild(a0)			; pointer to subsprite object
+
+Hel_Action:	; Routine 2
+		rts
+	;	bsr.w	Hel_RotateSpikes
+	;	bra.w	Hel_ChkDel			; Clownacy DisplaySprite Fix
 
 ; ||||||||||||||| S U B	R O U T	I N E |||||||||||||||||||||||||||||||||||||||
 
@@ -95,28 +117,16 @@ locret_7DA6:
 ; ===========================================================================
 
 Hel_ChkDel:
-		offscreen.s	Hel_DelAll		; ProjectFM S3K Objects Manager
+		out_of_range.s	Hel_Delete		; ProjectFM S3K Objects Manager
 		move.w	#priority3,d0			; RetroKoH/Devon S3K+ Priority Manager
 		bra.w	DisplaySprite2			; Display sprites
 ; ===========================================================================
 
-Hel_DelAll:
-		moveq	#0,d2
-		lea		obSubtype(a0),a2 ; move helix length to a2
-		move.b	(a2)+,d2	; move helix length to d2
-		subq.b	#2,d2
-		bcs.w	DeleteObject
-
-Hel_DelLoop:
-		moveq	#0,d0
-		move.b	(a2)+,d0
-		lsl.w	#object_size_bits,d0
-		addi.l	#v_objspace&$FFFFFF,d0
-		movea.l	d0,a1		; get child address
-		bsr.w	DeleteChild	; delete object
-		dbf		d2,Hel_DelLoop ; repeat d2 times (helix length)
-
 Hel_Delete:	; Routine 6
+		move.w	obRespawnNo(a0),d0
+		beq.w	DeleteObject
+		movea.w	d0,a2
+		bclr	#7,(a2)
 		bra.w	DeleteObject
 ; ===========================================================================
 
