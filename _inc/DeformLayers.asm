1343064189d78ddd59387e4540fd244bc271a321
KoH <cmgaudet2010@gmail.com>
2025-10-17 09:57:41 +0700

    Reverted part of the last commit because of an annoying bug.

--- _inc/DeformLayers.asm
+++ _inc/DeformLayers.asm
@@ -18,13 +18,14 @@ DeformLayers:
 		clr.w	(v_bg3_scroll_flags).w
 		bsr.w	ScrollHoriz
 
-;	if ActiveDeathSequence				; RetroKoH Active Death Sequence Mod
-;		cmpi.b	#6,(v_player+obRoutine).w	; has Sonic just died?
-;		bhs.s	.novertical					; if yes, branch and deform only
-;	endif
+; Adding this causes a LZ bug. Removing this causes a game-wide bug
+	if ActiveDeathSequence				; RetroKoH Active Death Sequence Mod
+		cmpi.b	#6,(v_player+obRoutine).w	; has Sonic just died?
+		bhs.s	.novertical					; if yes, branch and deform only
+	endif
 		bsr.w	ScrollVertical
 
-;	.novertical:
+	.novertical:
 		bsr.w	DynamicLevelEvents
 		move.w	(v_screenposy).w,(v_scrposy_vdp).w
 		move.w	(v_bgscreenposy).w,(v_bgscrposy_vdp).w
