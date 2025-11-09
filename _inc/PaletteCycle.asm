31cdd1a397071491a1802ff328d696560d6e3d2c
KoH <cmgaudet2010@gmail.com>
2025-11-09 13:26:25 +0700

    Optimize SBZ Palette Cycling

--- _inc/PaletteCycle.asm
+++ _inc/PaletteCycle.asm
@@ -200,10 +200,10 @@ locret_1AC6:
 
 
 PalCycle_SBZ:
-		lea		(Pal_SBZCycList1).l,a2
+		lea		Pal_SBZCycList1(pc),a2
 		tst.b	(v_act).w
 		beq.s	loc_1ADA
-		lea		(Pal_SBZCycList2).l,a2
+		lea		Pal_SBZCycList2(pc),a2
 
 loc_1ADA:
 		lea		(v_pal_buffer).w,a1
@@ -272,6 +272,48 @@ loc_1B52:
 locret_1B64:
 		rts	
 ; End of function PalCycle_SBZ
+; ===========================================================================
+
+; ---------------------------------------------------------------------------
+; Scrap Brain Zone palette cycling script
+; ---------------------------------------------------------------------------
+
+mSBZh:	macro {INTLABEL},{GLOBALSYMBOLS}
+__LABEL__:
+	dc.w ((__LABEL___end-__LABEL__-2)/6)-1
+	endm
+
+mSBZp:	macro duration,colours,sourceAddress,destinationPaletteIndex
+	dc.b duration, colours
+	dc.w sourceAddress, v_palette+destinationPaletteIndex*2
+	endm
+
+; duration in frames, number of colours, palette address, RAM address
+
+Pal_SBZCycList1: mSBZh
+	mSBZp	 7, 8,Pal_SBZCyc1,$28
+	mSBZp	13, 8,Pal_SBZCyc2,$29
+	mSBZp	14, 8,Pal_SBZCyc3,$37
+	mSBZp	11, 8,Pal_SBZCyc5,$38
+	mSBZp	 7, 8,Pal_SBZCyc6,$39
+	mSBZp	28,16,Pal_SBZCyc7,$3F
+	mSBZp	 3, 3,Pal_SBZCyc8,$3C
+	mSBZp	 3, 3,Pal_SBZCyc8+2,$3D
+	mSBZp	 3, 3,Pal_SBZCyc8+4,$3E
+Pal_SBZCycList1_end:
+	even
+
+Pal_SBZCycList2: mSBZh
+	mSBZp	 7, 8,Pal_SBZCyc1,$28
+	mSBZp	13, 8,Pal_SBZCyc2,$29
+	mSBZp	 9, 8,Pal_SBZCyc9,$38
+	mSBZp	 7, 8,Pal_SBZCyc6,$39
+	mSBZp	 3, 3,Pal_SBZCyc8,$3C
+	mSBZp	 3, 3,Pal_SBZCyc8+2,$3D
+	mSBZp	 3, 3,Pal_SBZCyc8+4,$3E
+Pal_SBZCycList2_end:
+	even
+; ===========================================================================
 
 	if SuperMod
 ; ---------------------------------------------------------------------------
@@ -323,6 +365,7 @@ PalCycle_SuperSonic:
 .return:
 		rts
 ; ===========================================================================
+
 ; loc_2188: PalCycle_SuperSonic_revert:
 .revert:	; runs the fade in transition backwards
 		; run frame timer
