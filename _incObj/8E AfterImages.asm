d2e4a1db3ef9ef34d75ba65f4d0b95b66f64cf6d
KoH <cmgaudet2010@gmail.com>
2025-11-27 15:30:39 +0700

    Minor annotations to Objects 8E and 8F

--- _incObj/8E AfterImages.asm
+++ _incObj/8E AfterImages.asm
@@ -6,30 +6,30 @@
 
 AfterImages:
 		tst.b	obRoutine(a0)
-		bne.s	AfterImages_Main
+		bne.s	AfterImages_Display
 
-AfterImages_Init:
+AfterImages_Main:
 		addq.b	#2,obRoutine(a0)
 	; More compact code section by RetroKoH
 		lea		(v_player).w,a1					; followerobj1: Pulls from v_player
-		cmpi.b	#2,obSubtype(a0)
+		tst.b	obSubtype(a0)
 		beq.s	.skip
 		lea		(v_followobject).w,a1			; followerobj2: Pulls from v_followobject
 
-.skip:
+	.skip:
 		move.l	obMap(a1),obMap(a0)				; Character obMap = afterimage obMap
 		move.w	obGfx(a1),obGfx(a0)				; use player 1's art
 		move.w	#priority2,obPriority(a0)
 		move.w	#$1818,obHeight(a0)				; Height and Width
 		move.b	#4,obRender(a0)	
 
-AfterImages_Main:
+AfterImages_Display:
 		moveq	#$C,d1							; This will be subtracted from Pos_table_index, giving the object an older entry
 		btst	#0,(v_framebyte).w				; Even frame? (Think of it as 'every other number' logic)
 		beq.s	.evenframe						; If so, branch
 		moveq	#$14,d1							; On every other frame, use a different number to subtract, giving the object an even older entry
 
-.evenframe:
+	.evenframe:
 		move.w	(v_trackpos).w,d0
 		lea		(v_tracksonic).w,a1
 		sub.b	d1,d0
@@ -43,11 +43,11 @@ AfterImages_Main:
 		cmpi.b	#8,obRoutine(a1)				; first check if player is already dead (and level is set to reset)
 		beq.s	.delete							; if so, delete
 		
-		cmpi.b	#2,obSubtype(a0)
+		tst.b	obSubtype(a0)
 		beq.s	.skip
 		lea		(v_followobject).w,a1			; followerobj2: Pulls from v_followobject
 
-.skip:
+	.skip:
 		tst.b	(v_debuguse).w
 		bne.s	.norender						; don't display in Debug Mode
 
@@ -59,15 +59,18 @@ AfterImages_Main:
 		tst.b	(v_player+obShoes).w			; check	time remaining
 		beq.s	.delete							; don't render if not Super or no shoes
 
-.render:
+	.render:
 		move.l	obMap(a1),obMap(a0)				; Use player's current obMap - added by hitaxas	   
 		move.b	obFrame(a1),obFrame(a0)			; Use player's current obFrame
 		move.b	obRender(a1),obRender(a0)		; Use player's current obRender
 		move.w	obPriority(a1),obPriority(a0)	; Use player's current obPriority
 		jmp		(DisplaySprite).l
+; ===========================================================================
 
-.norender:
+	.norender:
 		rts
+; ===========================================================================
 	
-.delete:
+	.delete:
 		jmp		(DeleteObject).l				; Destroy if no speed shoes
+; ===========================================================================
