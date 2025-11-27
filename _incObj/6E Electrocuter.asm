ea4d5b549a08860dd3acd0baac1532af48a3968a
KoH <cmgaudet2010@gmail.com>
2025-11-27 23:41:34 +0700

    Rewrote Electrocuter (Hivebrain)
    This version's routines are slightly more efficient. It also only runs AnimateSprite when zapping, and loads collision type a bit faster.

--- _incObj/6E Electrocuter.asm
+++ _incObj/6E Electrocuter.asm
@@ -1,14 +1,19 @@
 ; ---------------------------------------------------------------------------
 ; Object 6E - electrocution orbs (SBZ)
+; Rewritten based on S1Squared
 ; ---------------------------------------------------------------------------
 
-elec_freq = objoff_34		; frequency
-
 Electro:
-	; LavaGaming Object Routine Optimization
-		tst.b	obRoutine(a0)
-		bne.s	Elec_Shock
-	; Object Routine Optimization End
+		moveq	#0,d0
+		move.b	obRoutine(a0),d0
+		jmp		Elec_Index(pc,d0.w)
+; ===========================================================================
+Elec_Index:
+		bra.s Elec_Main
+		bra.s Elec_Wait
+		bra.s Elec_Zap
+		bra.s Elec_Reset
+; ===========================================================================
 
 Elec_Main:	; Routine 0
 		addq.b	#2,obRoutine(a0)
@@ -20,30 +25,44 @@ Elec_Main:	; Routine 0
 
 		bset	#shPropLightning,obShieldProp(a0)	; Negated by Lightning Shield
 
-		moveq	#0,d0
-		move.b	obSubtype(a0),d0					; read object type
+		moveq	#$F,d0
+		and.b	obSubtype(a0),d0					; read object subtype (cap at $F)
 		lsl.w	#4,d0								; multiply by $10
 		subq.w	#1,d0
-		move.w	d0,elec_freq(a0)
+		move.w	d0,obElecOrb_ZapRate(a0)			; set zap frequency
+; ---------------------------------------------------------------------------
 
-Elec_Shock:	; Routine 2
+Elec_Wait:	; Routine 2
 		move.w	(v_framecount).w,d0
-		and.w	elec_freq(a0),d0					; is it time to zap?
-		bne.s	.animate							; if not, branch
+		and.w	obElecOrb_ZapRate(a0),d0			; is it time to zap?
+		bne.s	Elec_Display						; if not, branch
 
-		move.b	#1,obAnim(a0)						; run "zap" animation
-		tst.b	obRender(a0)
-		bpl.s	.animate
+		tst.b	obRender(a0)						; is object on-screen?
+		bpl.s	Elec_Display						; if not, branch
 		move.w	#sfx_Electric,d0
-		jsr		(QueueSound2).w				; play electricity sound
+		jsr		(QueueSound2).w						; play electricity sound
+		addq.b	#2,obRoutine(a0)					; -> Elec_Zap
+		clr.b	obAniFrame(a0)						; reset animation
+; ---------------------------------------------------------------------------
 
-.animate:
+Elec_Zap:	; Routine 4
 		lea		Ani_Elec(pc),a1
-		jsr		(AnimateSprite).w
-		clr.b	obColType(a0)
-		cmpi.b	#4,obFrame(a0)						; is 4th frame displayed?
-		bne.s	.display							; if not, branch
-		move.b	#(colHarmful|colSz_72x8),obColType(a0)					; if yes, make object hurt Sonic
+		jsr		(AnimateSprite).w					; only animate if zapping
+		moveq	#0,d0
+		move.b	obFrame(a0),d0
+		move.b	Elec_Hurt(pc,d0.w),obColType(a0)	; convert frame id to collision type
 
-.display:
+Elec_Display:
 		jmp		(RememberState).l
+; ===========================================================================
+
+; table used for quicker obColType assignment
+Elec_Hurt:	dc.b 0, 0, 0, 0, (colHarmful|colSz_72x8), 0
+		even
+; ===========================================================================
+
+Elec_Reset:	; Routine 6
+		subq.b	#4,obRoutine(a0)					; -> Elec_Wait
+		clr.b	obColType(a0)
+		bra.s	Elec_Wait
+; ===========================================================================
