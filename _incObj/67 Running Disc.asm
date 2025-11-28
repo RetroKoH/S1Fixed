da94cfb48c4ba8b3ba14cd2998b49c4bd649a15b
KoH <cmgaudet2010@gmail.com>
2025-11-29 01:24:15 +0700

    Annotated and Optimized some SBZ objects
    Obj67 and 68 in particular have been improved, thanks to S1Squared and S1 SCE

--- _incObj/67 Running Disc.asm
+++ _incObj/67 Running Disc.asm
@@ -1,12 +1,19 @@
 ; ---------------------------------------------------------------------------
 ; Object 67 - disc that	you run	around (SBZ)
+; Optimizations based on S1Squared and Sonic Clean Engine
+;
+; subtypes:
+;	%SSSSRRRR
+;	SSSS - rotation speed (1-7 = clockwise; 8-$F = anticlockwise)
+;	RRRR - radius (see Disc_Radii)
 ; ---------------------------------------------------------------------------
 
-disc_origX = objoff_32		; original x-axis position
-disc_origY = objoff_30		; original y-axis position
-disc_spot_distance = objoff_34
-disc_radius = objoff_38
-disc_sonic_attached = objoff_3A
+; ===========================================================================
+
+Disc_Radii:
+		dc.b $18, $48
+		dc.b $10, $38
+; ===========================================================================
 
 RunningDisc:
 	; LavaGaming Object Routine Optimization
@@ -15,128 +22,43 @@ RunningDisc:
 	; Object Routine Optimization End
 
 Disc_Main:	; Routine 0
-		addq.b	#2,obRoutine(a0)
+		addq.b	#2,obRoutine(a0)					; -> Disc_Action
 		move.l	#Map_Disc,obMap(a0)
 		move.w	#make_art_tile(ArtTile_SBZ_Disc,2,1),obGfx(a0)
 		move.b	#4,obRender(a0)
-		move.w	#priority4,obPriority(a0)	; RetroKoH/Devon S3K+ Priority Manager
+		move.w	#priority4,obPriority(a0)			; RetroKoH/Devon S3K+ Priority Manager
 		move.b	#8,obDispWid(a0)
-		move.w	obX(a0),disc_origX(a0)
-		move.w	obY(a0),disc_origY(a0)
-		move.b	#$18,disc_spot_distance(a0)
-		move.b	#$48,disc_radius(a0)
-		move.b	obSubtype(a0),d1 ; get object type
-		andi.b	#$F,d1		; read only the	2nd digit
-		beq.s	.typeis0	; branch if 0
-		move.b	#$10,disc_spot_distance(a0)
-		move.b	#$38,disc_radius(a0)
+		move.w	obX(a0),obDisc_StartX(a0)
+		move.w	obY(a0),obDisc_StartY(a0)
 
-.typeis0:
-		move.b	obSubtype(a0),d1 ; get object type
-		andi.b	#$F0,d1		; read only the	1st digit
-		ext.w	d1
-		asl.w	#3,d1
-		move.w	d1,objoff_36(a0)
-		move.b	obStatus(a0),d0
-		ror.b	#2,d0
-		andi.b	#$C0,d0
-		move.b	d0,obAngle(a0)
+		moveq	#0,d0
+		move.b	obSubtype(a0),d0					; get object subtype
+		moveq	#$F,d1
+		and.b	d0,d1								; read only the low nybble
+		add.b	d1,d1
+		lea		Disc_Radii(pc,d1.w),a2
+		move.b	(a2)+,obDisc_RadiusInner(a0)		; unused smaller disc
+		move.b	(a2)+,obDisc_RadiusOuter(a0)
+
+		andi.b	#$F0,d0								; read only the	high nybble
+		ext.w	d0
+		asl.w	#3,d0								; multiply by 8
+		move.w	d0,obDisc_Rotation(a0)				; set rotation speed (only $200 is used)
+		move.b	obStatus(a0),d0						; get object status
+		ror.b	#2,d0								; move x/yflip bits to top
+		andi.b	#$C0,d0								; read only those
+		move.b	d0,obAngle(a0)						; use as starting angle
 
 Disc_Action:	; Routine 2
-		bsr.w	Disc_MoveSonic
-		bsr.w	Disc_MoveSpot
-		bra.w	Disc_ChkDel
-; ===========================================================================
-
-Disc_MoveSonic:
-		moveq	#0,d2
-		move.b	disc_radius(a0),d2
-		move.w	d2,d3
-		add.w	d3,d3
-		lea		(v_player).w,a1
-		move.w	obX(a1),d0
-		sub.w	disc_origX(a0),d0
-		add.w	d2,d0
-		cmp.w	d3,d0
-		bhs.s	.detach
-		move.w	obY(a1),d1
-		sub.w	disc_origY(a0),d1
-		add.w	d2,d1
-		cmp.w	d3,d1
-		bhs.s	.detach
-		btst	#staAir,obStatus(a1)
-		beq.s	.attach
-		clr.b	disc_sonic_attached(a0)
-		rts	
-; ===========================================================================
-; loc_155A8:
-.detach:
-		tst.b	disc_sonic_attached(a0)
-		beq.s	.return
-		clr.b	obOnWheel(a1)
-		clr.b	disc_sonic_attached(a0)
-; locret_155B6:
-.return:
-		rts	
-; ===========================================================================
-; loc_155B8:
-.attach:
-		tst.b	disc_sonic_attached(a0)
-		bne.s	loc_155E2
-		move.b	#1,disc_sonic_attached(a0)
-		btst	#staSpin,obStatus(a1)
-		bne.s	loc_155D0
-		move.b	#aniID_Walk,obAnim(a1)
-
-loc_155D0:
-		bclr	#staPush,obStatus(a1)
-		move.b	#aniID_Run,obPrevAni(a1) ; restart Sonic's animation
-		move.b	#1,obOnWheel(a1)
-
-loc_155E2:
-		move.w	obInertia(a1),d0
-		tst.w	objoff_36(a0)
-		bpl.s	loc_15608
-		cmpi.w	#-$400,d0
-		ble.s	loc_155FA
-		move.w	#-$400,obInertia(a1)
-		rts	
-; ===========================================================================
-
-loc_155FA:
-		cmpi.w	#-$F00,d0
-		bge.s	locret_15606
-		move.w	#-$F00,obInertia(a1)
-
-locret_15606:
-		rts	
-; ===========================================================================
-
-loc_15608:
-		cmpi.w	#$400,d0
-		bge.s	loc_15616
-		move.w	#$400,obInertia(a1)
-		rts	
-; ===========================================================================
-
-loc_15616:
-		cmpi.w	#$F00,d0
-		ble.s	locret_15622
-		move.w	#$F00,obInertia(a1)
-
-locret_15622:
-		rts	
-; ===========================================================================
-
-Disc_MoveSpot:
-		move.w	objoff_36(a0),d0
-		add.w	d0,obAngle(a0)
+		bsr.s	Disc_MoveSonic
+		move.w	obDisc_Rotation(a0),d1
+		add.w	d1,obAngle(a0)						; update angle (d1 is obDisc_Rotation)
 		move.b	obAngle(a0),d0
-		jsr	(CalcSine).w
-		move.w	disc_origY(a0),d2
-		move.w	disc_origX(a0),d3
+		jsr		(CalcSine).w						; convert to sine/cosine
+		move.w	obDisc_StartY(a0),d2
+		move.w	obDisc_StartX(a0),d3
 		moveq	#0,d4
-		move.b	disc_spot_distance(a0),d4
+		move.b	obDisc_RadiusInner(a0),d4
 		lsl.w	#8,d4
 		move.l	d4,d5
 		muls.w	d0,d4
@@ -145,14 +67,98 @@ Disc_MoveSpot:
 		swap	d5
 		add.w	d2,d4
 		add.w	d3,d5
-		move.w	d4,obY(a0)
+		move.w	d4,obY(a0)							; update position
 		move.w	d5,obX(a0)
+
+Disc_ChkDel:
+		offscreen.s	.delete,obDisc_StartX(a0)
+		jmp	(DisplaySprite).l
+
+	.delete:
+		jmp	(DeleteObject).l
+; ===========================================================================
+
+; ---------------------------------------------------------------------------
+; Subroutine to detect collision with disc and set Sonic's inertia
+; ---------------------------------------------------------------------------
+
+Disc_MoveSonic:
+		moveq	#0,d2
+		move.b	obDisc_RadiusOuter(a0),d2
+		move.w	d2,d3
+		add.w	d3,d3
+		lea		(v_player).w,a1						; a1 = object RAM of Sonic
+		move.w	obX(a1),d0
+		sub.w	obDisc_StartX(a0),d0
+		add.w	d2,d0
+		cmp.w	d3,d0
+		bhs.s	.detach
+		move.w	obY(a1),d1
+		sub.w	obDisc_StartY(a0),d1
+		add.w	d2,d1
+		cmp.w	d3,d1
+		bhs.s	.detach								; branch if Sonic is outside the range of the disc
+		btst	#staAir,obStatus(a1)				; is Sonic in the air?
+		beq.s	.attach								; if not, branch
+		clr.b	obDisc_SonicAttached(a0)
 		rts	
 ; ===========================================================================
 
-Disc_ChkDel:
-		offscreen.s	.delete,disc_origX(a0)
-		jmp	(DisplaySprite).l
+	.detach:
+		tst.b	obDisc_SonicAttached(a0)			; was Sonic on the disc last frame?
+		beq.s	.skip_clear							; if not, branch
+		clr.b	obOnWheel(a1)
+		clr.b	obDisc_SonicAttached(a0)
 
-.delete:
-		jmp	(DeleteObject).l
+	.skip_clear:
+		rts	
+; ===========================================================================
+
+	.attach:
+		tst.b	obDisc_SonicAttached(a0)			; was Sonic on the disc last frame?
+		bne.s	.skip_init							; if yes, branch
+		move.b	#1,obDisc_SonicAttached(a0)			; set flag for Sonic being on the disc
+		btst	#staSpin,obStatus(a1)				; is Sonic jumping?
+		bne.s	.jumping							; if yes, branch
+		move.b	#aniID_Walk,obAnim(a1)				; use walking animation
+
+	.jumping:
+		bclr	#staPush,obStatus(a1)
+		move.b	#aniID_Run,obPrevAni(a1)			; restart Sonic's animation
+		move.b	#1,obOnWheel(a1)					; keep Sonic stuck to disc until he jumps
+
+	.skip_init:
+		move.w	obInertia(a1),d0
+		tst.w	obDisc_Rotation(a0)					; check rotation direction (only $200 is used)
+		bpl.s	.clockwise							; branch if positive
+
+		cmpi.w	#-$400,d0
+		ble.s	.chk_max_neg
+		move.w	#-$400,obInertia(a1)				; set minimum inertia on disc
+		rts	
+; ===========================================================================
+
+	.chk_max_neg:
+		cmpi.w	#-$F00,d0
+		bge.s	.exit
+		move.w	#-$F00,obInertia(a1)				; set maximum inertia on disc
+
+	.exit:
+		rts	
+; ===========================================================================
+
+	.clockwise:
+		cmpi.w	#$400,d0
+		bge.s	.chk_max
+		move.w	#$400,obInertia(a1)					; set minimum inertia on disc
+		rts	
+; ===========================================================================
+
+	.chk_max:
+		cmpi.w	#$F00,d0
+		ble.s	.exit2
+		move.w	#$F00,obInertia(a1)					; set maximum inertia on disc
+
+	.exit2:
+		rts	
+; ===========================================================================
--- _incObj/6A Saws and Pizza Cutters.asm
+++ _incObj/6A Saws and Pizza Cutters.asm
@@ -2,10 +2,6 @@
 ; Object 6A - ground saws and pizza cutters (SBZ)
 ; ---------------------------------------------------------------------------
 
-saw_origY = objoff_38		; original y-axis position
-saw_origX = objoff_3A		; original x-axis position
-saw_here = objoff_3D		; flag set when the ground saw appears
-
 Saws:
 	; LavaGaming Object Routine Optimization
 		tst.b	obRoutine(a0)
@@ -17,178 +13,203 @@ Saw_Main:	; Routine 0
 		move.l	#Map_Saw,obMap(a0)
 		move.w	#make_art_tile(ArtTile_SBZ_Saw,2,0),obGfx(a0)
 		move.b	#4,obRender(a0)
-		move.w	#priority4,obPriority(a0)	; RetroKoH/Devon S3K+ Priority Manager
+		move.w	#priority4,obPriority(a0)		; RetroKoH/Devon S3K+ Priority Manager
 		move.b	#$20,obDispWid(a0)
-		move.w	obX(a0),saw_origX(a0)
-		move.w	obY(a0),saw_origY(a0)
-		cmpi.b	#3,obSubtype(a0)			; is object a ground saw?
-		bhs.s	Saw_Action					; if yes, branch
+		move.w	obX(a0),obSaw_StartX(a0)
+		move.w	obY(a0),obSaw_StartY(a0)
+		cmpi.b	#3,obSubtype(a0)				; is object a ground saw?
+		bhs.s	Saw_Action						; if yes, branch
 		move.b	#(colHarmful|colSz_24x24_2),obColType(a0)
+; ---------------------------------------------------------------------------
 
 Saw_Action:	; Routine 2
-		moveq	#7,d0						; get last digit of subtype (sans bit 3)
-		and.b	obSubtype(a0),d0			; SCE optimization
-		beq.s	.type00						; skip if subtype 00
+		moveq	#7,d0							; get last digit of subtype (sans bit 3)
+		and.b	obSubtype(a0),d0				; SCE optimization
+		beq.s	.type00							; skip if subtype 00 (doesn't move; unused)
 		add.w	d0,d0
 		move.w	Saw_Index-2(pc,d0.w),d1
 		jsr		Saw_Index(pc,d1.w)
 
-.type00:
-		offscreen.s	.delete,saw_origX(a0)	; PFM S3K OBJ
-		jmp		(DisplayAndCollision).l		; S3K TouchResponse
+	.type00:
+		offscreen.s	.delete,obSaw_StartX(a0)	; PFM S3K OBJ
+		jmp		(DisplayAndCollision).l			; S3K TouchResponse
 
-.delete:
-		jmp	(DeleteObject).l
+	.delete:
+		jmp		(DeleteObject).l
 ; ===========================================================================
 Saw_Index:		offsetTable
 	; pizza cutters
-		offsetTableEntry.w	Saw_Type01
-		offsetTableEntry.w	Saw_Type02
+		offsetTableEntry.w	Saw_Pizza_Sideways	; moves side-to-side
+		offsetTableEntry.w	Saw_Pizza_UpDown	; moves up and down
 	; ground saws
-		offsetTableEntry.w	Saw_Type03
-		offsetTableEntry.w	Saw_Type04
+		offsetTableEntry.w	Saw_Ground_Right	; moves right
+		offsetTableEntry.w	Saw_Ground_Left		; moves left (unused)
 ; ===========================================================================
 
-Saw_Type01:
-		move.w	#$60,d1
+; Type 1
+Saw_Pizza_Sideways:
+		moveq	#$60,d1
 		moveq	#0,d0
 		move.b	(v_oscillate+$E).w,d0
 		btst	#staFlipX,obStatus(a0)
-		beq.s	.noflip01
+		beq.s	.no_flip
 		neg.w	d0
 		add.w	d1,d0
 
-.noflip01:
-		move.w	saw_origX(a0),d1
+	.no_flip:
+		move.w	obSaw_StartX(a0),d1
 		sub.w	d0,d1
-		move.w	d1,obX(a0)	; move saw sideways
+		move.w	d1,obX(a0)					; move saw sideways
 
-		subq.b	#1,obTimeFrame(a0)
-		bpl.s	.sameframe01
-		move.b	#2,obTimeFrame(a0) ; time between frame changes
-		bchg	#0,obFrame(a0)	; change frame
+		; wait
+		subq.b	#1,obTimeFrame(a0)			; decrement timer
+		bpl.s	.same_frame					; if time remains, branch
+		addq.b	#2+1,obTimeFrame(a0)		; time between frame changes
+		bchg	#0,obFrame(a0)				; change frame
 
-.sameframe01:
-		tst.b	obRender(a0)
-		bpl.s	.nosound01
-		move.w	(v_framecount).w,d0
-		andi.w	#$F,d0
-		bne.s	.nosound01
+	.same_frame:
+		tst.b	obRender(a0)				; is object visible on the screen?
+		bpl.s	.return						; if not, branch
+
+		; play sfx
+		moveq	#$F,d0
+		and.w	(v_framecount).w,d0
+		bne.s	.return
 		move.w	#sfx_Saw,d0
-		jsr	(QueueSound2).w		; play saw sound
+		jmp		(QueueSound2).w				; play saw sound
+; ===========================================================================
 
-.nosound01:
+	.return:
 		rts	
 ; ===========================================================================
 
-Saw_Type02:
-		move.w	#$30,d1
+; Type 2
+Saw_Pizza_UpDown:
+		moveq	#$30,d1
 		moveq	#0,d0
 		move.b	(v_oscillate+6).w,d0
 		btst	#staFlipX,obStatus(a0)
-		beq.s	.noflip02
+		beq.s	.no_flip
 		neg.w	d0
 		addi.w	#$80,d0
 
-.noflip02:
-		move.w	saw_origY(a0),d1
+	.no_flip:
+		move.w	obSaw_StartY(a0),d1
 		sub.w	d0,d1
-		move.w	d1,obY(a0)	; move saw vertically
-		subq.b	#1,obTimeFrame(a0)
-		bpl.s	.sameframe02
-		move.b	#2,obTimeFrame(a0)
-		bchg	#0,obFrame(a0)
+		move.w	d1,obY(a0)					; move saw vertically
 
-.sameframe02:
-		tst.b	obRender(a0)
-		bpl.s	.nosound02
-		move.b	(v_oscillate+6).w,d0
-		cmpi.b	#$18,d0
-		bne.s	.nosound02
+		; wait
+		subq.b	#1,obTimeFrame(a0)			; decrement timer
+		bpl.s	.same_frame					; if time remains, branch
+		addq.b	#2+1,obTimeFrame(a0)		; reset timer to 2 frames
+		bchg	#0,obFrame(a0)				; change frame
+
+	.same_frame:
+		tst.b	obRender(a0)				; is object visible on the screen?
+		bpl.s	.return						; if not, branch
+
+		; play sfx
+		cmpi.b	#$18,(v_oscillate+6).w
+		bne.s	.return
 		move.w	#sfx_Saw,d0
-		jsr		(QueueSound2).w		; play saw sound
+		jmp		(QueueSound2).w				; play saw sound
+; ===========================================================================
 
-.nosound02:
+	.return:
 		rts	
 ; ===========================================================================
 
-Saw_Type03:
-		tst.b	saw_here(a0)				; has the saw appeared already?
+; Type 3
+Saw_Ground_Right:
+		tst.b	obSaw_GroundFlag(a0)		; has the saw appeared already?
 		bne.s	.here03						; if yes, branch
 
+		; check
 		move.w	(v_player+obX).w,d0
 		subi.w	#$C0,d0
-		bcs.s	.nosaw03x
+		bcs.s	.nosaw03x					; branch if Sonic is within 192px of left edge boundary
 		sub.w	obX(a0),d0
-		bcs.s	.nosaw03x
+		bcs.s	.nosaw03x					; branch if saw is < 192px to Sonic's left
 		move.w	(v_player+obY).w,d0
 		subi.w	#$80,d0
 		cmp.w	obY(a0),d0
-		bhs.s	.nosaw03y
+		bhs.s	.nosaw03y					; branch if saw is > 128px above Sonic
 		addi.w	#$100,d0
 		cmp.w	obY(a0),d0
-		blo.s	.nosaw03y
-		move.b	#1,saw_here(a0)
+		bcs.s	.nosaw03y					; branch if saw is > 128px below Sonic
+
+		; set
+		move.b	#1,obSaw_GroundFlag(a0)		; flag object as already loaded
 		move.w	#$600,obVelX(a0) 			; move object to the right
 		move.b	#(colHarmful|colSz_24x24_2),obColType(a0)
 		move.b	#2,obFrame(a0)
 		move.w	#sfx_Saw,d0
-		jsr		(QueueSound2).w		; play saw sound
+		jsr		(QueueSound2).w				; play saw sound
 
-.nosaw03x:
-		addq.l	#4,sp
+	.nosaw03x:
+		addq.l	#4,sp						; exit from object
 
-.nosaw03y:
+	.nosaw03y:
 		rts	
 ; ===========================================================================
 
-.here03:
+	.here03:
 		jsr		(SpeedToPos_XOnly).l
-		move.w	obX(a0),saw_origX(a0)
-		subq.b	#1,obTimeFrame(a0)
-		bpl.s	.sameframe03
-		move.b	#2,obTimeFrame(a0)
-		bchg	#0,obFrame(a0)
+		move.w	obX(a0),obSaw_StartX(a0)
 
-.sameframe03:
+		; wait
+		subq.b	#1,obTimeFrame(a0)			; decrement frame timer
+		bpl.s	.sameframe03				; branch if time remains
+		addq.b	#2+1,obTimeFrame(a0)		; reset timer
+		bchg	#0,obFrame(a0)				; change frame
+
+	.sameframe03:
 		rts	
 ; ===========================================================================
 
-Saw_Type04:
-		tst.b	saw_here(a0)
-		bne.s	.here04
+; Type 4
+Saw_Ground_Left:
+		tst.b	obSaw_GroundFlag(a0)		; has the saw appeared already?
+		bne.s	.here04						; if yes, branch
+
+		; check
 		move.w	(v_player+obX).w,d0
 		addi.w	#$E0,d0
 		sub.w	obX(a0),d0
-		bcc.s	.nosaw04x
+		bcc.s	.nosaw04x					; branch if saw is > 224px right of Sonic 
 		move.w	(v_player+obY).w,d0
 		subi.w	#$80,d0
 		cmp.w	obY(a0),d0
-		bhs.s	.nosaw04y
+		bhs.s	.nosaw04y					; branch if saw is > 128px above Sonic
 		addi.w	#$100,d0
 		cmp.w	obY(a0),d0
-		blo.s	.nosaw04y
-		move.b	#1,saw_here(a0)
-		move.w	#-$600,obVelX(a0) 		; move object to the left
+		bcs.s	.nosaw04y					; branch if saw is > 128px below Sonic
+
+		; set
+		move.b	#1,obSaw_GroundFlag(a0)		; flag object as already loaded
+		move.w	#-$600,obVelX(a0) 			; move object to the left
 		move.b	#(colHarmful|colSz_24x24_2),obColType(a0)
 		move.b	#2,obFrame(a0)
 		move.w	#sfx_Saw,d0
-		jsr	(QueueSound2).w		; play saw sound
+		jsr		(QueueSound2).w				; play saw sound
 
-.nosaw04x:
-		addq.l	#4,sp
+	.nosaw04x:
+		addq.l	#4,sp						; exit from object
 
-.nosaw04y:
+	.nosaw04y:
 		rts	
 ; ===========================================================================
 
-.here04:
+	.here04:
 		jsr		(SpeedToPos_XOnly).l
-		move.w	obX(a0),saw_origX(a0)
-		subq.b	#1,obTimeFrame(a0)
-		bpl.s	.sameframe04
-		move.b	#2,obTimeFrame(a0)
-		bchg	#0,obFrame(a0)
+		move.w	obX(a0),obSaw_StartX(a0)
 
-.sameframe04:
-		rts	
+		; wait
+		subq.b	#1,obTimeFrame(a0)			; decrement frame timer
+		bpl.s	.sameframe04				; branch if time remains
+		addq.b	#2+1,obTimeFrame(a0)		; reset timer
+		bchg	#0,obFrame(a0)				; change frame
+
+	.sameframe04:
+		rts
+; ===========================================================================
