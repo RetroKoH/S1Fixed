b4edb25a45d3bd97f86652ee9cc603752a60bd84
KoH <cmgaudet2010@gmail.com>
2025-11-28 10:20:48 +0700

    Flamethrower Rewrite
    Some slight optimizations, one OST byte removed. Gonna test this and compare to the old version.

--- _incObj/6D Flamethrower.asm
+++ _incObj/6D Flamethrower.asm
@@ -1,58 +1,83 @@
 ; ---------------------------------------------------------------------------
-; Object 6D - flame thrower (SBZ)
+; Object 6D - flamethrower (SBZ)
+; Rewritten based on S1Squared
+
+; subtypes:
+;	%FFFFWWWW
+;	FFFF - time flame is on (*32 frames)
+;	WWWW - wait time between flames (*32 frames)
 ; ---------------------------------------------------------------------------
 
 Flamethrower:
-	; LavaGaming Object Routine Optimization
-		tst.b	obRoutine(a0)
-		bne.s	Flame_Action
-	; Object Routine Optimization End
+		moveq	#0,d0
+		move.b	obRoutine(a0),d0
+		move.w	Flame_Index(pc,d0.w),d1
+		jmp		Flame_Index(pc,d1.w)
+; ===========================================================================
+
+Flame_Index:	offsetTable
+		offsetTableEntry.w Flame_Main
+		offsetTableEntry.w Flame_On
+		offsetTableEntry.w Flame_Off
+		offsetTableEntry.w Flame_Off2
+; ===========================================================================
 
 Flame_Main:	; Routine 0
-		addq.b	#2,obRoutine(a0)
+		addq.b	#2,obRoutine(a0)				; -> Flame_On
 		move.l	#Map_Flame,obMap(a0)
 		move.w	#make_art_tile(ArtTile_SBZ_Flamethrower,0,1),obGfx(a0)
 		ori.b	#4,obRender(a0)
 		move.w	#priority1,obPriority(a0)		; RetroKoH/Devon S3K+ Priority Manager
-		move.w	obY(a0),objoff_30(a0)			; store obY (gets overwritten later though)
 		move.b	#$C,obDispWid(a0)
 
 		bset	#shPropFlame,obShieldProp(a0)	; Negated by Flame Shield
 
 		move.b	obSubtype(a0),d0
-		andi.w	#$F0,d0					; read 1st digit of object type
-		add.w	d0,d0					; multiply by 2
-		move.w	d0,objoff_30(a0)
-		move.w	d0,objoff_32(a0)		; set flaming time
+		andi.w	#$F0,d0							; read high nybble of object subtype
+		add.w	d0,d0							; multiply by 2
+		move.w	d0,obFlame_WaitTime(a0)
+		move.w	d0,obFlame_OnTime(a0)			; set flaming time
 		move.b	obSubtype(a0),d0
-		andi.w	#$F,d0					; read 2nd digit of object type
-		lsl.w	#5,d0					; multiply by $20
-		move.w	d0,objoff_34(a0)		; set pause time
-		move.b	#$A,objoff_36(a0)
-		btst	#staFlipY,obStatus(a0)
-		beq.s	Flame_Action
-		move.b	#2,obAnim(a0)
-		move.b	#$15,objoff_36(a0)
-
-Flame_Action:	; Routine 2
-		subq.w	#1,objoff_30(a0)	; subtract 1 from time
-		bpl.s	loc_E57A	; if time remains, branch
-		move.w	objoff_34(a0),objoff_30(a0)	; begin	pause time
-		bchg	#0,obAnim(a0)
-		beq.s	loc_E57A
-		move.w	objoff_32(a0),objoff_30(a0)	; begin	flaming	time
+		andi.w	#$F,d0							; read low nybble of object subtype
+		lsl.w	#5,d0							; multiply by $20
+		move.w	d0,obFlame_OffTime(a0)			; set pause time
+		moveq	#2,d0							; 2 = yFlip bitmask (set this up in Constants.asm)
+		and.b	obStatus(a0),d0					; get yflip flag (0 or 2)
+		move.b	d0,obAnim(a0)					; use as animation
 		move.w	#sfx_Flamethrower,d0
-		jsr		(QueueSound2).w ; play flame sound
+		jsr		(QueueSound2).w					; play flame sound
+; ---------------------------------------------------------------------------
 
-loc_E57A:
+Flame_On:	; Routine 2
 		lea		Ani_Flame(pc),a1
 		jsr		(AnimateSprite).w
-		clr.b	obColType(a0)
-		move.b	objoff_36(a0),d0
-		cmp.b	obFrame(a0),d0
-		bne.s	Flame_ChkDel
-		move.b	#(colHarmful|colSz_12x24),obColType(a0)
+		cmpi.b	#$A,obAniFrame(a0)						; final frame is now hard-coded here
+		bne.s	.wait_anim								; branch if not at final animation frame
+		move.b	#(colHarmful|colSz_12x24),obColType(a0)	; make flame harmful
 
-Flame_ChkDel:
-		offscreen.w	DeleteObject	; ProjectFM S3K Object Manager
-		bra.w	DisplayAndCollision	; S3K TouchResponse
+	.wait_anim:
+		subq.w	#1,obFlame_WaitTime(a0)			; decrement timer
+		bpl.w	RememberState				; if time remains, branch
+		move.w	obFlame_OffTime(a0),obFlame_WaitTime(a0)	; begin pause time
+		bchg	#0,obAnim(a0)					; switch between on/off animations
+		addq.b	#2,obRoutine(a0)				; -> Flame_Off
+		clr.b	obColType(a0)					; make object harmless
+		bra.w	RememberState
+; ===========================================================================
+
+Flame_Off:		; Routine 4
+		lea		Ani_Flame(pc),a1
+		jsr		(AnimateSprite).w
+
+Flame_Off2:		; Routine 6
+		subq.w	#1,obFlame_WaitTime(a0)			; decrement timer
+		bpl.w	RememberState					; if time remains, branch
+		move.w	obFlame_OffTime(a0),obFlame_WaitTime(a0)	; begin pause time
+		bchg	#0,obAnim(a0)					; switch between on/off animations
+		subq.b	#4,obRoutine(a0)				; -> Flame_On
+		tst.b	obRender(a0)
+		bpl.w	RememberState					; branch if off screen
+		move.w	#sfx_Flamethrower,d0
+		jsr		(QueueSound2).w					; play flame sound
+		bra.w	RememberState
+; ===========================================================================
