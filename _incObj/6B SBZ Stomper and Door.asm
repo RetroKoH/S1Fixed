8ab8928ad2a8dbacb1403a12a3a24122edc779bf
KoH <cmgaudet2010@gmail.com>
2025-11-28 23:04:29 +0700

    Annotated Obj6B

--- _incObj/6B SBZ Stomper and Door.asm
+++ _incObj/6B SBZ Stomper and Door.asm
@@ -1,10 +1,9 @@
 ; ---------------------------------------------------------------------------
 ; Object 6B - stomper and sliding door (SBZ)
+; Stompers
+; Thin Horizontal Sliding Doors triggered by switches
+; Large Sliding Platform in SBZ3
 ; ---------------------------------------------------------------------------
-
-sto_origX = objoff_34		; original x-axis position
-sto_origY = objoff_30		; original y-axis position
-sto_active = objoff_38		; flag set when a switch is pressed
 
 ; ===========================================================================
 Sto_Var:
@@ -22,83 +21,85 @@ ScrapStomp:
 		bne.w	Sto_Action
 	; Object Routine Optimization End
 
-
 Sto_Main:	; Routine 0
-		addq.b	#2,obRoutine(a0)
+		addq.b	#2,obRoutine(a0)		; -> Sto_Action
 		moveq	#0,d0
 		move.b	obSubtype(a0),d0
-		lsr.w	#2,d0				; subtype div 4
-		andi.w	#$1C,d0
-		lea		Sto_Var(pc,d0.w),a3
+		lsr.w	#2,d0					; subtype div 4
+		andi.w	#$1C,d0					; read only high nybble without bit 7
+		lea		Sto_Var(pc,d0.w),a3		; get variables from list
 		move.b	(a3)+,obDispWid(a0)
 		move.b	(a3)+,obHeight(a0)
 		lsr.w	#2,d0
-		move.b	d0,obFrame(a0)
+		move.b	d0,obFrame(a0)			; high nybble without bit 7 = frame
 		move.l	#Map_Stomp,obMap(a0)
 		move.w	#make_art_tile(ArtTile_SBZ_Moving_Block_Short,1,0),obGfx(a0)
-		cmpi.b	#id_LZ,(v_zone).w	; check if level is LZ/SBZ3
-		bne.s	.isSBZ12			; if not, branch
-		bset	#0,(v_obj6B).w
-		beq.s	.isSBZ3
+		cmpi.b	#id_LZ,(v_zone).w		; check if level is LZ/SBZ3
+		bne.s	.is_SBZ_12				; if not, branch
+		bset	#0,(v_obj6B).w			; flag object as loaded
+		beq.s	.sbz3_init				; branch if not previously loaded
 
-.chkdel:
+	.chkdel:
 		move.w	obRespawnAddr(a0),d0	; get address in respawn table
-		beq.s	.delete				; if it's zero, don't remember object
-		movea.w	d0,a2				; load address into a2
-		bclr	#7,(a2)				; clear respawn table entry, so object can be loaded again
+		beq.s	.delete					; if it's zero, don't remember object
+		movea.w	d0,a2					; load address into a2
+		bclr	#7,(a2)					; clear respawn table entry, so object can be loaded again
 
-.delete:
+	.delete:
 		jmp		(DeleteObject).l
 ; ===========================================================================
 
-.isSBZ3:
+	.sbz3_init:
 		move.w	#make_art_tile(ArtTile_LZ_Block_2,2,0),obGfx(a0)
-		cmpi.w	#$A80,obX(a0)
-		bne.s	.isSBZ12
-		move.w	obRespawnAddr(a0),d0		; get address in respawn table
-		beq.s	.isSBZ12				; if it's zero, don't remember object
+		cmpi.w	#$A80,obX(a0)			; is object in its starting position?
+		bne.s	.skip_sbz3_init			; if not, branch
+		move.w	obRespawnAddr(a0),d0	; get address in respawn table
+		beq.s	.skip_sbz3_init			; if it's zero, don't remember object
 		movea.w	d0,a2					; load address into a2
 		btst	#0,(a2)
-		beq.s	.isSBZ12
+		beq.s	.skip_sbz3_init
 		clr.b	(v_obj6B).w
 		bra.s	.chkdel
 ; ===========================================================================
 
-.isSBZ12:
+	.is_SBZ_12:
+	.skip_sbz3_init:
 		ori.b	#4,obRender(a0)
 		move.w	#priority4,obPriority(a0)	; RetroKoH/Devon S3K+ Priority Manager
-		move.w	obX(a0),sto_origX(a0)
-		move.w	obY(a0),sto_origY(a0)
+		move.w	obX(a0),obStomp_StartX(a0)
+		move.w	obY(a0),obStomp_StartY(a0)
 		moveq	#0,d0
 		move.b	(a3)+,d0
-		move.w	d0,objoff_3C(a0)
+		move.w	d0,obStomp_DistToMove(a0)	; set distance to move
 		moveq	#0,d0
-		move.b	obSubtype(a0),d0
-		bpl.s	Sto_Action
-		andi.b	#$F,d0
-		move.b	d0,objoff_3E(a0)
-		move.b	(a3),obSubtype(a0)
-		cmpi.b	#5,(a3)
-		bne.s	.chkgone
+		move.b	obSubtype(a0),d0			; get subtype
+		bpl.s	Sto_Action					; branch if 0-$7F
+
+		andi.b	#$F,d0						; read only low nybble
+		move.b	d0,obStomp_ButtonNum(a0)	; copy to obStomp_ButtonNum
+		move.b	(a3),obSubtype(a0)			; update subtype with value from list
+		cmpi.b	#5,(a3)						; is object the huge sliding platform from SBZ3? (5)
+		bne.s	.chkgone					; if not, branch
 		bset	#4,obRender(a0)
 
-.chkgone:
-		move.w	obRespawnAddr(a0),d0	; get address in respawn table
-		beq.s	Sto_Action			; if it's zero, don't remember object
-		movea.w	d0,a2				; load address into a2
-		bclr	#7,(a2)				; clear respawn table entry, so object can be loaded again
+	.chkgone:
+		move.w	obRespawnAddr(a0),d0		; get address in respawn table
+		beq.s	Sto_Action					; if it's zero, don't remember object
+		movea.w	d0,a2						; load address into a2
+		bclr	#7,(a2)						; clear respawn table entry, so object can be loaded again
+; ---------------------------------------------------------------------------
 
 Sto_Action:	; Routine 2
-		move.w	obX(a0),-(sp)
-		moveq	#$F,d0				; get last digit of subtype
-		and.b	obSubtype(a0),d0	; SCE optimization
-		beq.s	.type00				; skip if subtype 00
+		move.w	obX(a0),-(sp)				; save x pos to stack
+		moveq	#$F,d0						; get last digit of subtype
+		and.b	obSubtype(a0),d0			; SCE optimization
+		beq.s	.type00						; skip if subtype 00
 		add.w	d0,d0
 		move.w	Sto_Index-2(pc,d0.w),d1
 		jsr		Sto_Index(pc,d1.w)
 
-.type00:
-		move.w	(sp)+,d4
+	.type00:
+		move.w	(sp)+,d4					; retrieve x pos from stack
 		tst.b	obRender(a0)
 		bpl.s	.chkdel
 		moveq	#0,d1
@@ -110,212 +111,223 @@ Sto_Action:	; Routine 2
 		addq.w	#1,d3
 		jsr		(SolidObject).l
 
-.chkdel:
-		offscreen.s	.chkgone,sto_origX(a0)
+	.chkdel:
+		offscreen.s	.chkgone,obStomp_StartX(a0)
 		jmp	(DisplaySprite).l
 
-.chkgone:
+	.chkgone:
 		cmpi.b	#id_LZ,(v_zone).w
 		bne.s	.delete
 		clr.b	(v_obj6B).w
 		move.w	obRespawnAddr(a0),d0	; get address in respawn table
-		beq.s	.delete				; if it's zero, don't remember object
-		movea.w	d0,a2				; load address into a2
-		bclr	#7,(a2)				; clear respawn table entry, so object can be loaded again
+		beq.s	.delete					; if it's zero, don't remember object
+		movea.w	d0,a2					; load address into a2
+		bclr	#7,(a2)					; clear respawn table entry, so object can be loaded again
 
-.delete:
+	.delete:
 		jmp	(DeleteObject).l
 ; ===========================================================================
 Sto_Index:	offsetTable
-		offsetTableEntry.w Sto_Type01
-		offsetTableEntry.w Sto_Type02
-		offsetTableEntry.w Sto_Type03
-		offsetTableEntry.w Sto_Type04
-		offsetTableEntry.w Sto_Type05
+		offsetTableEntry.w Sto_SlideOpen
+		offsetTableEntry.w Sto_SlideClose
+		offsetTableEntry.w Sto_Drop_RiseSlow
+		offsetTableEntry.w Sto_Drop_RiseFast
+		offsetTableEntry.w Sto_SlideDiagonal
 ; ===========================================================================
 
-Sto_Type01:
-		tst.b	sto_active(a0)
-		bne.s	.isactive01
+; Type 1
+; Horizonal door, opens when button (ost_stomp_button_num) is pressed
+Sto_SlideOpen:
+		tst.b	obStomp_ButtonFlag(a0)		; has door been activated?
+		bne.s	.isactive01					; if yes, branch
 		lea	(f_switch).w,a2
 		moveq	#0,d0
-		move.b	objoff_3E(a0),d0
-		btst	#0,(a2,d0.w)
-		beq.s	.loc_15DC2
-		move.b	#1,sto_active(a0)
+		move.b	obStomp_ButtonNum(a0),d0
+		btst	#0,(a2,d0.w)				; has button been pressed?
+		beq.s	.update_pos					; if not, branch
+		move.b	#1,obStomp_ButtonFlag(a0)
 
-.isactive01:
-		move.w	objoff_3C(a0),d0
-		cmp.w	objoff_3A(a0),d0
-		beq.s	.loc_15DE0
-		addq.w	#2,objoff_3A(a0)
+	.isactive01:
+		move.w	obStomp_DistToMove(a0),d0	; get target distance
+		cmp.w	obStomp_DistMoved(a0),d0	; has door moved that distance?
+		beq.s	.finished01					; if yes, branch
+		addq.w	#2,obStomp_DistMoved(a0)	; move 2px
 
-.loc_15DC2:
-		move.w	objoff_3A(a0),d0
+	.update_pos:
+		move.w	obStomp_DistMoved(a0),d0
 		btst	#staFlipX,obStatus(a0)
 		beq.s	.noflip01
 		neg.w	d0
 		addi.w	#$80,d0
 
-.noflip01:
-		move.w	sto_origX(a0),d1
-		sub.w	d0,d1
-		move.w	d1,obX(a0)
+	.noflip01:
+		move.w	obStomp_StartX(a0),d1		; get initial x pos
+		sub.w	d0,d1						; apply difference
+		move.w	d1,obX(a0)					; update position
 		rts	
 ; ===========================================================================
 
-.loc_15DE0:
-		addq.b	#1,obSubtype(a0)
-		move.w	#$B4,objoff_36(a0)
-		clr.b	sto_active(a0)
+	.finished01:
+		addq.b	#1,obSubtype(a0)			; change to subtype 2
+		move.w	#$B4,obStomp_WaitTime(a0)	; set timer to 3 seconds
+		clr.b	obStomp_ButtonFlag(a0)		; clear active flag
 		move.w	obRespawnAddr(a0),d0		; get address in respawn table
-		beq.s	.loc_15DC2				; if it's zero, don't remember object
-		movea.w	d0,a2					; load address into a2
+		beq.s	.update_pos					; if it's zero, don't remember object
+		movea.w	d0,a2						; load address into a2
 		bset	#0,(a2)
-		bra.s	.loc_15DC2
+		bra.s	.update_pos
 ; ===========================================================================
 
-Sto_Type02:
-		tst.b	sto_active(a0)
-		bne.s	.isactive02
-		subq.w	#1,objoff_36(a0)
-		bne.s	.loc_15E1E
-		move.b	#1,sto_active(a0)
+; Type 2
+; Horizonal door, returns to its original position after 3 seconds
+Sto_SlideClose:
+		tst.b	obStomp_ButtonFlag(a0)		; has door been activated?
+		bne.s	.isactive02					; if yes, branch
+		subq.w	#1,obStomp_WaitTime(a0)		; decrement timer
+		bne.s	.update_pos					; branch if time remains
+		move.b	#1,obStomp_ButtonFlag(a0)
 
-.isactive02:
-		tst.w	objoff_3A(a0)
-		beq.s	.loc_15E3C
-		subq.w	#2,objoff_3A(a0)
+	.isactive02:
+		tst.w	obStomp_DistMoved(a0)		; has door reached its original position?
+		beq.s	.finished02					; if yes, branch
+		subq.w	#2,obStomp_DistMoved(a0)	; move back 2px
 
-.loc_15E1E:
-		move.w	objoff_3A(a0),d0
+	.update_pos:
+		move.w	obStomp_DistMoved(a0),d0
 		btst	#staFlipX,obStatus(a0)
 		beq.s	.noflip02
 		neg.w	d0
 		addi.w	#$80,d0
 
-.noflip02:
-		move.w	sto_origX(a0),d1
-		sub.w	d0,d1
-		move.w	d1,obX(a0)
+	.noflip02:
+		move.w	obStomp_StartX(a0),d1		; get initial x pos
+		sub.w	d0,d1						; apply difference
+		move.w	d1,obX(a0)					; update position
 		rts	
 ; ===========================================================================
 
-.loc_15E3C:
-		subq.b	#1,obSubtype(a0)
-		clr.b	sto_active(a0)
-		move.w	obRespawnAddr(a0),d0	; get address in respawn table
-		beq.s	.loc_15E1E			; if it's zero, don't remember object
-		movea.w	d0,a2				; load address into a2
+	.finished02:
+		subq.b	#1,obSubtype(a0)			; change back to type 1
+		clr.b	obStomp_ButtonFlag(a0)		; clear active flag
+		move.w	obRespawnAddr(a0),d0		; get address in respawn table
+		beq.s	.update_pos					; if it's zero, don't remember object
+		movea.w	d0,a2						; load address into a2
 		bclr	#0,(a2)
-		bra.s	.loc_15E1E
+		bra.s	.update_pos
 ; ===========================================================================
 
-Sto_Type03:
-		tst.b	sto_active(a0)
+; Type 3
+; Stomper, drops quickly and rises slowly
+Sto_Drop_RiseSlow:
+		tst.b	obStomp_ButtonFlag(a0)
 		bne.s	.isactive03
-		tst.w	objoff_3A(a0)
-		beq.s	.loc_15E6A
-		subq.w	#1,objoff_3A(a0)
-		bra.s	.loc_15E8E
+		tst.w	obStomp_DistMoved(a0)		; has stomper started moving?
+		beq.s	.wait						; if not, branch
+		subq.w	#1,obStomp_DistMoved(a0)	; move up 1px
+		bra.s	.update_pos
 ; ===========================================================================
 
-.loc_15E6A:
-		subq.w	#1,objoff_36(a0)
-		bpl.s	.loc_15E8E
-		move.w	#$3C,objoff_36(a0)
-		move.b	#1,sto_active(a0)
+	.wait:
+		subq.w	#1,obStomp_WaitTime(a0)		; decrement wait time
+		bpl.s	.update_pos					; branch if time remains
+		move.w	#$3C,obStomp_WaitTime(a0)	; set wait time to 1 second
+		move.b	#1,obStomp_ButtonFlag(a0)
 
-.isactive03:
-		addq.w	#8,objoff_3A(a0)
-		move.w	objoff_3A(a0),d0
-		cmp.w	objoff_3C(a0),d0
-		bne.s	.loc_15E8E
-		clr.b	sto_active(a0)
+	.isactive03:
+		addq.w	#8,obStomp_DistMoved(a0)	; move down 8px
+		move.w	obStomp_DistMoved(a0),d0
+		cmp.w	obStomp_DistToMove(a0),d0	; has stomper reached maximum distance?
+		bne.s	.update_pos					; if not, branch
+		clr.b	obStomp_ButtonFlag(a0)
 
-.loc_15E8E:
-		move.w	objoff_3A(a0),d0
+	.update_pos:
+		move.w	obStomp_DistMoved(a0),d0
 		btst	#staFlipX,obStatus(a0)
 		beq.s	.noflip03
 		neg.w	d0
 		addi.w	#$38,d0
 
-.noflip03:
-		move.w	sto_origY(a0),d1
-		add.w	d0,d1
-		move.w	d1,obY(a0)
+	.noflip03:
+		move.w	obStomp_StartY(a0),d1		; get initial y pos
+		add.w	d0,d1						; apply difference
+		move.w	d1,obY(a0)					; update position
 		rts	
 ; ===========================================================================
 
-Sto_Type04:
-		tst.b	sto_active(a0)
+; Type 4
+; Stomper, drops quickly and rises quickly
+Sto_Drop_RiseFast:
+		tst.b	obStomp_ButtonFlag(a0)
 		bne.s	.isactive04
-		tst.w	objoff_3A(a0)
-		beq.s	.loc_15EBE
-		subq.w	#8,objoff_3A(a0)
-		bra.s	.loc_15EF0
+		tst.w	obStomp_DistMoved(a0)		; has stomper started moving?
+		beq.s	.wait						; if not, branch
+		subq.w	#8,obStomp_DistMoved(a0)	; move up 8px
+		bra.s	.update_pos
 ; ===========================================================================
 
-.loc_15EBE:
-		subq.w	#1,objoff_36(a0)
-		bpl.s	.loc_15EF0
-		move.w	#$3C,objoff_36(a0)
-		move.b	#1,sto_active(a0)
+	.wait:
+		subq.w	#1,obStomp_WaitTime(a0)		; decrement wait time
+		bpl.s	.update_pos					; branch if time remains
+		move.w	#60,obStomp_WaitTime(a0)	; set wait time to 1 second
+		move.b	#1,obStomp_ButtonFlag(a0)
 
-.isactive04:
-		move.w	objoff_3A(a0),d0
-		cmp.w	objoff_3C(a0),d0
-		beq.s	.loc_15EE0
-		addq.w	#8,objoff_3A(a0)
-		bra.s	.loc_15EF0
+	.isactive04:
+		move.w	obStomp_DistMoved(a0),d0
+		cmp.w	obStomp_DistToMove(a0),d0	; has stomper reached maximum distance?
+		beq.s	.wait2						; if not, branch
+		addq.w	#8,obStomp_DistMoved(a0)	; move down 8px
+		bra.s	.update_pos
 ; ===========================================================================
 
-.loc_15EE0:
-		subq.w	#1,objoff_36(a0)
-		bpl.s	.loc_15EF0
-		move.w	#$3C,objoff_36(a0)
-		clr.b	sto_active(a0)
+	.wait2:
+		subq.w	#1,obStomp_WaitTime(a0)		; decrement wait time
+		bpl.s	.update_pos					; branch if time remains
+		move.w	#$3C,obStomp_WaitTime(a0)	; set wait time to 1 second
+		clr.b	obStomp_ButtonFlag(a0)
 
-.loc_15EF0:
-		move.w	objoff_3A(a0),d0
+	.update_pos:
+		move.w	obStomp_DistMoved(a0),d0
 		btst	#staFlipX,obStatus(a0)
 		beq.s	.noflip04
 		neg.w	d0
 		addi.w	#$38,d0
 
-.noflip04:
-		move.w	sto_origY(a0),d1
-		add.w	d0,d1
-		move.w	d1,obY(a0)
+	.noflip04:
+		move.w	obStomp_StartY(a0),d1		; get initial y pos
+		add.w	d0,d1						; apply difference
+		move.w	d1,obY(a0)					; update position
 		rts	
 ; ===========================================================================
 
-Sto_Type05:
-		tst.b	sto_active(a0)
-		bne.s	.loc_15F3E
+; Type 5
+; Huge sliding door from SBZ3
+Sto_SlideDiagonal:
+		tst.b	obStomp_ButtonFlag(a0)		; has door been activated?
+		bne.s	.update_pos					; if yes, branch
 		lea		(f_switch).w,a2
 		moveq	#0,d0
-		move.b	objoff_3E(a0),d0
-		btst	#0,(a2,d0.w)
-		beq.s	.locret_15F5C
-		move.b	#1,sto_active(a0)
-		move.w	obRespawnAddr(a0),d0	; get address in respawn table
-		beq.s	.loc_15F3E			; if it's zero, don't remember object
-		movea.w	d0,a2				; load address into a2
-		bclr	#7,(a2)				; clear respawn table entry, so object can be loaded again
+		move.b	obStomp_ButtonNum(a0),d0
+		btst	#0,(a2,d0.w)				; has relevant button been pressed?
+		beq.s	.exit						; if not, branch
+		move.b	#1,obStomp_ButtonFlag(a0)	; set active flag
+		move.w	obRespawnAddr(a0),d0		; get address in respawn table
+		beq.s	.update_pos					; if it's zero, don't remember object
+		movea.w	d0,a2						; load address into a2
+		bclr	#7,(a2)						; clear respawn table entry, so object can be loaded again
 
-.loc_15F3E:
-		subi.l	#$10000,obX(a0)
-		addi.l	#$8000,obY(a0)
-		move.w	obX(a0),sto_origX(a0)
-		cmpi.w	#$980,obX(a0)
-		beq.s	.loc_15F5E
+.update_pos:
+		subi.l	#$10000,obX(a0)				; move left 1px
+		addi.l	#$8000,obY(a0)				; move down 0.5px
+		move.w	obX(a0),obStomp_StartX(a0)
+		cmpi.w	#$980,obX(a0)				; has door reached target position?
+		beq.s	.finish						; if yes, branch
 
-.locret_15F5C:
+.exit:
 		rts	
 ; ===========================================================================
 
-.loc_15F5E:
-		clr.b	obSubtype(a0)
-		clr.b	sto_active(a0)
+.finish:
+		clr.b	obSubtype(a0)				; change type to 0 (doesn't move)
+		clr.b	obStomp_ButtonFlag(a0)
 		rts	
+; ===========================================================================
