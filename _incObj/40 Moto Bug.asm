a85a6bad6b99b36ff49dbf945b0bad1eb59af101
KoH <cmgaudet2010@gmail.com>
2025-12-08 12:00:32 +0700

    Added mod to fix Green Newtrons deleting themselves (KoH)
    Also annotated a few more objects based on Hivebrain's disassembly.

--- _incObj/40 Moto Bug.asm
+++ _incObj/40 Moto Bug.asm
@@ -2,9 +2,6 @@
 ; Object 40 - Moto Bug enemy (GHZ)
 ; ---------------------------------------------------------------------------
 
-motoTime = objoff_30
-motoSmokedelay = objoff_33
-
 MotoBug:
 	; RetroKoH Object Routine Optimization
 		moveq	#0,d0
@@ -31,19 +28,20 @@ Moto_Main:	; Routine 0
 		move.b	#(colEnemy|colSz_20x16),obColType(a0)
 		bsr.w	ObjectFall_YOnly
 		bsr.w	ObjFloorDist
-		tst.w	d1
-		bpl.s	.notonfloor
+		tst.w	d1							; has motobug hit the floor?
+		bpl.s	.notonfloor					; if not, branch
 		add.w	d1,obY(a0)					; match	object's position with the floor
-		clr.w	obVelY(a0)
-		addq.b	#2,obRoutine(a0)			; goto Moto_Action next
+		clr.w	obVelY(a0)					; stop falling
+		addq.b	#2,obRoutine(a0)			; -> Moto_Action
 		bchg	#staFlipX,obStatus(a0)
 
-.notonfloor:
+	.notonfloor:
 		rts	
 ; ===========================================================================
 
-.smoke:
-		addq.b	#4,obRoutine(a0) ; goto Moto_Animate below
+	.smoke:
+		addq.b	#4,obRoutine(a0)			; -> Moto_Animate
+; ---------------------------------------------------------------------------
 
 Moto_Animate:	; Routine 4
 		lea		Ani_Moto(pc),a1
@@ -54,53 +52,56 @@ Moto_Animate:	; Routine 4
 Moto_Action:	; Routine 2
 	; LavaGaming/RetroKoH Object Routine Optimization
 		tst.b	ob2ndRout(a0)
-		bne.s	.findfloor
+		bne.s	Moto_FindFloor
 	; Object Routine Optimization End
 
-.move:
-		subq.w	#1,motoTime(a0)	; subtract 1 from pause	time
-		bpl.s	.wait		; if time remains, branch
-		addq.b	#2,ob2ndRout(a0)
-		move.w	#-$100,obVelX(a0) ; move object to the left
+Moto_Move:
+		subq.w	#1,obMoto_TurnTime(a0)		; decrement wait timer
+		bpl.s	.wait						; if time remains, branch
+		addq.b	#2,ob2ndRout(a0)			; -> Moto_FindFloor
+		move.w	#-$100,obVelX(a0)			; move object to the left
 		move.b	#1,obAnim(a0)
-		bchg	#staFlipX,obStatus(a0)
-		bne.s	.wait
-		neg.w	obVelX(a0)	; change direction
+		bchg	#staFlipX,obStatus(a0)		; is Motobug facing right?
+		bne.s	.wait						; if not, branch
+		neg.w	obVelX(a0)					; change direction to the right
 
-.wait:
+	.wait:
 		lea		Ani_Moto(pc),a1
 		jsr		(AnimateSprite).w
 		bra.s	RememberState
 ; ===========================================================================
 
-.findfloor:
+Moto_FindFloor:
 		bsr.w	SpeedToPos_XOnly
-		jsr		(ObjFloorDist).l
+		jsr		(ObjFloorDist).l			; d1 = distance to floor
 		cmpi.w	#-8,d1
 		blt.s	.pause
 		cmpi.w	#$C,d1
-		bge.s	.pause
-		add.w	d1,obY(a0)	; match	object's position with the floor
-		subq.b	#1,motoSmokedelay(a0)
-		bpl.s	.nosmoke
-		move.b	#$F,motoSmokedelay(a0)
+		bge.s	.pause						; branch if object is more than 11px above or 8px below floor
+
+		add.w	d1,obY(a0)					; align to floor
+		subq.b	#1,obMoto_SmokeDelay(a0)	; decrement time between smoke puffs
+		bpl.s	.nosmoke					; branch if time remains
+		move.b	#$F,obMoto_SmokeDelay(a0)	; reset timer
 		bsr.w	FindFreeObj
-		bne.s	.nosmoke
-		_move.b	#id_MotoBug,obID(a1) ; load exhaust smoke object
+		bne.s	.nosmoke					; branch if object slot not found
+
+		_move.b	#id_MotoBug,obID(a1)		; load exhaust smoke object
 		move.w	obX(a0),obX(a1)
 		move.w	obY(a0),obY(a1)
 		move.b	obStatus(a0),obStatus(a1)
 		move.b	#2,obAnim(a1)
 		bra.s	.nosmoke
 
-.pause:
-		subq.b	#2,ob2ndRout(a0)
-		move.w	#59,motoTime(a0)	; set pause time to 1 second
-		clr.w	obVelX(a0)			; stop the object moving
+	.pause:
+		subq.b	#2,ob2ndRout(a0)			; -> Moto_Move
+		move.w	#59,obMoto_TurnTime(a0)		; set pause time to 1 second
+		clr.w	obVelX(a0)					; stop the object moving
 		clr.b	obAnim(a0)
 
-.nosmoke:
+	.nosmoke:
 		lea		Ani_Moto(pc),a1
 		jsr		(AnimateSprite).w
+; ---------------------------------------------------------------------------
 
 		include	"_incObj/sub RememberState.asm"	; Moto_Action terminates in this file
