870fbc3ddf607c610f6161005270d5b04023a077
KoH <cmgaudet2010@gmail.com>
2025-12-09 22:29:33 +0700

    Annotations and bitclear optimizations
    bitclear optimizations by KoH

--- _incObj/4A Special Stage Entry (Unused).asm
+++ _incObj/4A Special Stage Entry (Unused).asm
@@ -11,8 +11,6 @@ Van_Index:
 		bra.s Van_Main
 		bra.s Van_RmvSonic
 		bra.s Van_LoadSonic
-
-van_time = objoff_30		; time for Sonic to disappear
 ; ===========================================================================
 
 Van_Main:	; Routine 0
@@ -20,38 +18,41 @@ Van_Main:	; Routine 0
 		beq.s	.isempty	; if yes, branch
 		rts	
 
-.isempty:
+	.isempty:
 		addq.b	#2,obRoutine(a0)
 		move.l	#Map_Vanish,obMap(a0)
 		move.b	#4,obRender(a0)
 		move.w	#priority1,obPriority(a0)	; RetroKoH/Devon S3K+ Priority Manager
 		move.b	#$38,obDispWid(a0)
 		move.w	#make_art_tile(ArtTile_Warp,0,0),obGfx(a0)
-		move.w	#120,van_time(a0) ; set time for Sonic's disappearance to 2 seconds
+		move.w	#120,obVanish_Timer(a0)		; set time for Sonic's disappearance to 2 seconds
+; ---------------------------------------------------------------------------
 
 Van_RmvSonic:	; Routine 2
 		move.w	(v_player+obX).w,obX(a0)
 		move.w	(v_player+obY).w,obY(a0)
 		move.b	(v_player+obStatus).w,obStatus(a0)
 		lea		Ani_Vanish(pc),a1
-		jsr		(AnimateSprite).w
-		cmpi.b	#2,obFrame(a0)
-		bne.s	.display
-		tst.b	(v_player).w
-		beq.s	.display
-		clr.b	(v_player).w	; remove Sonic
-		move.w	#sfx_SSGoal,d0
-		jsr		(QueueSound2).w	; play Special Stage "GOAL" sound
+		jsr		(AnimateSprite).w			; animate and goto Van_LoadSonic next
+		cmpi.b	#2,obFrame(a0)				; is final flash frame displayed?
+		bne.s	.display					; if not, branch
 
-.display:
+		tst.b	(v_player).w				; has Sonic already been removed?
+		beq.s	.display					; if yes, branch
+		clr.b	(v_player).w				; remove Sonic
+		move.w	#sfx_SSGoal,d0
+		jsr		(QueueSound2).w				; play Special Stage "GOAL" sound
+
+	.display:
 		jmp		(DisplaySprite).l
 ; ===========================================================================
 
 Van_LoadSonic:	; Routine 4
-		subq.w	#1,van_time(a0)	; subtract 1 from time
-		bne.s	.wait		; if time remains, branch
-		move.b	#id_SonicPlayer,(v_player).w ; load Sonic object
+		subq.w	#1,obVanish_Timer(a0)		; subtract 1 from time
+		bne.s	.wait						; if time remains, branch
+		move.b	#id_SonicPlayer,(v_player).w	; load Sonic object
 		jmp		(DeleteObject).l
 
-.wait:
-		rts	
+	.wait:
+		rts
+; ===========================================================================
