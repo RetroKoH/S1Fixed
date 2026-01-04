bcdcf69504af39f38e23a149157ec0e97c846d57
KoH <cmgaudet2010@gmail.com>
2026-01-04 15:23:41 +0700

    Rewrote Obj03 and 04 (KoH)
    The second rewrite for Obj04

--- _incObj/04 Auto Roll Tag.asm
+++ _incObj/04 Auto Roll Tag.asm
@@ -1,11 +1,11 @@
 ; ----------------------------------------------------------------------------
 ; Object 04 - Pinball mode enable/disable
-; Backported from Sonic 2's Obj84, and rewritten by RetroKoH
+; Backported from Sonic 2's Obj84; rewritten by RetroKoH
 ; ----------------------------------------------------------------------------
 
 AutoRollTag:
 		move.l	#Map_PathSwapper,obMap(a0)
-		move.w	#$27B2,obGfx(a0)			; change this
+		move.w	#make_art_tile(ArtTile_Ring,0,0),obGfx(a0)	; Like Pathswapper, but red
 		ori.b	#4,obRender(a0)
 		move.b	#$10,obDispWid(a0)
 		move.w	#priority5,obPriority(a0)	; RetroKoH/Devon S3K+ Priority Manager
@@ -14,19 +14,18 @@ AutoRollTag:
 		beq.s	ARoll_Init_CheckX
 
 ;ARoll_Init_CheckY:
-		obj_addr	#ARoll_MainY
 		andi.w	#7,d0
 		move.b	d0,obFrame(a0)
 		andi.w	#3,d0
 		add.w	d0,d0
 		move.w	ARoll_Sizes(pc,d0.w),obARoll_Radius(a0)
+		obj_addr	#ARoll_MainY
 		move.w	obY(a0),d1
 		lea		(v_player).w,a1
 		cmp.w	obY(a1),d1
 		bhs.w	ARoll_MainY
 		move.b	#1,obARoll_Flag(a0)
-		bsr.w	ARoll_MainY
-		bra.s	ARoll_ChkDel
+		bra.w	ARoll_MainY
 ; ===========================================================================
 
 ARoll_Sizes:
@@ -37,17 +36,31 @@ ARoll_Sizes:
 ; ===========================================================================
 
 ARoll_Init_CheckX:
-		obj_addr	#ARoll_MainX
 		andi.w	#3,d0
 		move.b	d0,obFrame(a0)
 		add.w	d0,d0
 		move.w	ARoll_Sizes(pc,d0.w),obARoll_Radius(a0)
+		obj_addr	#ARoll_MainX
 		move.w	obX(a0),d1
 		lea		(v_player).w,a1
 		cmp.w	obX(a1),d1
 		bhs.s	ARoll_MainX
 		move.b	#1,obARoll_Flag(a0)
-		bsr.w	ARoll_MainX
+		bra.s	ARoll_MainX
+; ===========================================================================
+
+ARoll_Disable:
+		move.b	#0,obAutoRollFlag(a1)		; disable auto roll mode
+		bra.w	ARoll_ChkDel
+; ===========================================================================
+
+ARoll_ChkRoll:
+; the original S2 code had a copy of the Chk Roll code
+; I went this route, in case there are modifications to ChkRoll, and to keep code cleaner
+		move.l	a0,-(sp)
+		movea.l	a1,a0						; move player to a0
+		bsr.w	Sonic_ChkRoll
+		movea.l	(sp)+,a0					; restore a0 = obj04
 ; ----------------------------------------------------------------------------
 
 ARoll_ChkDel:
@@ -74,14 +87,14 @@ ARoll_ChkDel:
 
 ARoll_MainX:
 		tst.w	(v_debuguse).w
-		bne.s	ARoll_Exit
+		bne.s	ARoll_ChkDel
 		move.w	obX(a0),d1
 		lea		obARoll_Flag(a0),a2			; a2 = obARoll_Flag
 		lea		(v_player).w,a1
 		tst.b	(a2)						; is obARoll_Flag set?
 		bne.s	ARoll_MainX_Alt				; if yes, branch
 		cmp.w	obX(a1),d1
-		bhi.s	ARoll_Exit
+		bhi.s	ARoll_ChkDel
 		move.b	#1,(a2)						; set obARoll_Flag
 		move.w	obY(a0),d2
 		move.w	d2,d3
@@ -90,18 +103,18 @@ ARoll_MainX:
 		add.w	d4,d3
 		move.w	obY(a1),d4
 		cmp.w	d2,d4
-		blt.s	ARoll_Exit
+		blt.s	ARoll_ChkDel
 		cmp.w	d3,d4
-		bge.s	ARoll_Exit
+		bge.s	ARoll_ChkDel
 		btst	#0,obRender(a0)
-		bne.s	ARoll_Disable
+		bne.w	ARoll_Disable
 		move.b	#1,obAutoRollFlag(a1)		; enable auto roll mode
-		bra.s	ARoll_ChkRoll
+		bra.w	ARoll_ChkRoll
 ; ===========================================================================
 
 ARoll_MainX_Alt:
 		cmp.w	obX(a1),d1
-		bls.s	ARoll_Exit
+		bls.w	ARoll_ChkDel
 		clr.b	(a2)						; clear obARoll_Flag
 		move.w	obY(a0),d2
 		move.w	d2,d3
@@ -110,42 +123,25 @@ ARoll_MainX_Alt:
 		add.w	d4,d3
 		move.w	obY(a1),d4
 		cmp.w	d2,d4
-		blt.s	ARoll_Exit
+		blt.w	ARoll_ChkDel
 		cmp.w	d3,d4
-		bge.s	ARoll_Exit
+		bge.w	ARoll_ChkDel
 		btst	#0,obRender(a0)
-		beq.s	ARoll_Disable
+		beq.w	ARoll_Disable
 		move.b	#1,obAutoRollFlag(a1)		; enable auto roll mode
-		bra.s	ARoll_ChkRoll
-; ===========================================================================
-
-ARoll_Disable:
-		move.b	#0,obAutoRollFlag(a1)		; disable auto roll mode
-
-ARoll_Exit:
-		rts
-; ===========================================================================
-
-ARoll_ChkRoll:
-; the original S2 code had a copy of the Chk Roll code
-; I went this route, in case there are modifications to ChkRoll, and to keep code cleaner
-		move.l	a0,-(sp)
-		movea.l	a1,a0						; move player to a0
-		bsr.w	Sonic_ChkRoll
-		movea.l	(sp)+,a0					; restore a0 = obj04
-		rts
+		bra.w	ARoll_ChkRoll
 ; ===========================================================================
 
 ARoll_MainY:
 		tst.w	(v_debuguse).w
-		bne.s	ARoll_Exit
+		bne.w	ARoll_ChkDel
 		move.w	obY(a0),d1
 		lea		obARoll_Flag(a0),a2			; a2 = obARoll_Flag
 		lea		(v_player).w,a1
 		tst.b	(a2)						; is obARoll_Flag set?
 		bne.s	ARoll_MainY_Alt				; if yes, branch
 		cmp.w	obY(a1),d1
-		bhi.s	ARoll_Exit
+		bhi.w	ARoll_ChkDel
 		move.b	#1,(a2)						; set obARoll_Flag
 		move.w	obX(a0),d2
 		move.w	d2,d3
@@ -154,18 +150,18 @@ ARoll_MainY:
 		add.w	d4,d3
 		move.w	obX(a1),d4
 		cmp.w	d2,d4
-		blo.s	ARoll_Exit
+		blo.w	ARoll_ChkDel
 		cmp.w	d3,d4
-		bhs.s	ARoll_Exit
+		bhs.w	ARoll_ChkDel
 		btst	#0,obRender(a0)
-		bne.s	ARoll_Disable
+		bne.w	ARoll_Disable
 		move.b	#1,obAutoRollFlag(a1)		; enable auto roll mode
 		bra.w	ARoll_ChkRoll
 ; ===========================================================================
 
 ARoll_MainY_Alt:
 		cmp.w	obY(a1),d1
-		bls.s	ARoll_Exit
+		bls.w	ARoll_ChkDel
 		clr.b	(a2)						; clear obARoll_Flag
 		move.w	obX(a0),d2
 		move.w	d2,d3
@@ -174,9 +170,9 @@ ARoll_MainY_Alt:
 		add.w	d4,d3
 		move.w	obX(a1),d4
 		cmp.w	d2,d4
-		blo.s	ARoll_Exit
+		blo.w	ARoll_ChkDel
 		cmp.w	d3,d4
-		bhs.s	ARoll_Exit
+		bhs.w	ARoll_ChkDel
 		btst	#0,obRender(a0)
 		beq.w	ARoll_Disable
 		move.b	#1,obAutoRollFlag(a1)		; enable auto roll mode
