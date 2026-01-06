dd2a60ff8335a13bc94568d7cab90f2be926d54b
KoH <cmgaudet2010@gmail.com>
2026-01-06 09:45:36 +0700

    Fixed SYZ Boss Spike and Optimized Ball Hog
    Obj20 removed. Ball Hog rewrite done by Hivebrain and RetroKoH.

--- sonic.asm
+++ sonic.asm
@@ -3239,7 +3239,6 @@ Ledge_SlopeData:
 
 		include	"_anim/Ball Hog.asm"
 		include	"_incObj/1E Ball Hog.asm"
-		include	"_incObj/20 Cannonball.asm"
 
 		include	"_incObj/24, 27 & 3F Explosions.asm"
 
