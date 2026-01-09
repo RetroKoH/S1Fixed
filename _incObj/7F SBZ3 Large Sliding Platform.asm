cb0d33c7f5b299146df2a58450f2c9f6cbd48895
KoH <cmgaudet2010@gmail.com>
2026-01-09 11:12:59 +0700

    Split SBZ3 Large Sliding Platform from Obj6B into Obj7F
    Not a clean break, but for now, it works.

---
+++ _incObj/7F SBZ3 Large Sliding Platform.asm
@@ -0,0 +1,129 @@
+; ---------------------------------------------------------------------------
+; Object 7F - large sliding platform (LZ/SBZ3)
+; ---------------------------------------------------------------------------
+
+SlidingPlatform:
+	; LavaGaming Object Routine Optimization
+		tst.b	obRoutine(a0)
+		bne.w	Slid_Action
+	; Object Routine Optimization End
+; ---------------------------------------------------------------------------
+
+Slid_Main:	; Routine 0
+		addq.b	#2,obRoutine(a0)		; -> Slid_Action
+		move.l	#Map_Stomp,obMap(a0)
+		move.w	#make_art_tile(ArtTile_LZ_Block_2,2,0),obGfx(a0)
+		move.b	#$80,obDispWid(a0)
+		move.b	#$40,obHeight(a0)		; TO-DO: Add obDispHgt so this always renders correctly
+		move.b	#4,obFrame(a0)			; hardset frame (We will make exclusive mappings)
+		bset	#0,(v_obj6B).w			; flag object as loaded
+		beq.s	.sbz3_init				; branch if not previously loaded
+
+	.chkdel:
+		move.w	obRespawnAddr(a0),d0	; get address in respawn table
+		beq.s	.delete					; if it's zero, don't remember object
+		movea.w	d0,a2					; load address into a2
+		bclr	#7,(a2)					; clear respawn table entry, so object can be loaded again
+
+	.delete:
+		jmp		(DeleteObject).l
+; ===========================================================================
+
+	.sbz3_init:
+		cmpi.w	#$A80,obX(a0)			; is object in its starting position?
+		bne.s	.skip_sbz3_init			; if not, branch
+		move.w	obRespawnAddr(a0),d0	; get address in respawn table
+		beq.s	.skip_sbz3_init			; if it's zero, don't remember object
+		movea.w	d0,a2					; load address into a2
+		btst	#0,(a2)
+		beq.s	.skip_sbz3_init
+		clr.b	(v_obj6B).w
+		bra.s	.chkdel
+; ===========================================================================
+
+	.skip_sbz3_init:
+		ori.b	#4,obRender(a0)
+		move.w	#priority4,obPriority(a0)	; RetroKoH/Devon S3K+ Priority Manager
+		move.w	obX(a0),obStomp_StartX(a0)
+		move.w	obY(a0),obStomp_StartY(a0)
+		moveq	#0,d0
+		move.b	obSubtype(a0),d0			; get subtype
+		bpl.s	Slid_Action					; branch if 0-$7F
+
+		andi.b	#$F,d0						; read only low nybble
+		move.b	d0,obStomp_ButtonNum(a0)	; copy to obStomp_ButtonNum
+		bset	#4,obRender(a0)
+
+	.chkgone:
+		move.w	obRespawnAddr(a0),d0		; get address in respawn table
+		beq.s	Slid_Action					; if it's zero, don't remember object
+		movea.w	d0,a2						; load address into a2
+		bclr	#7,(a2)						; clear respawn table entry, so object can be loaded again
+; ---------------------------------------------------------------------------
+
+Slid_Action:	; Routine 2
+		move.w	obX(a0),-(sp)				; save x pos to stack
+		tst.b	obSubtype(a0)				; should it slide?
+		beq.s	.type00						; if not, branch
+		bsr.w	Slid_SlideDiagonal
+
+	.type00:
+		move.w	(sp)+,d4					; retrieve x pos from stack
+		tst.b	obRender(a0)
+		bpl.s	.chkdel
+
+		; solid
+		moveq	#0,d1
+		move.b	obDispWid(a0),d1
+		addi.w	#$B,d1
+		moveq	#$40,d2						; height (jumping)
+		moveq	#$41,d3						; height (walking)
+		jsr		(SolidObject).l
+
+	.chkdel:
+		offscreen.s	.chkgone,obStomp_StartX(a0)
+		jmp	(DisplaySprite).l
+; ---------------------------------------------------------------------------
+
+	.chkgone:
+		clr.b	(v_obj6B).w
+		move.w	obRespawnAddr(a0),d0	; get address in respawn table
+		beq.s	.delete					; if it's zero, don't remember object
+		movea.w	d0,a2					; load address into a2
+		bclr	#7,(a2)					; clear respawn table entry, so object can be loaded again
+
+	.delete:
+		jmp	(DeleteObject).l
+; ===========================================================================
+
+Slid_SlideDiagonal:
+		tst.b	obStomp_ButtonFlag(a0)		; has door been activated?
+		bne.s	.update_pos					; if yes, branch
+		lea		(f_switch).w,a2
+		moveq	#0,d0
+		move.b	obStomp_ButtonNum(a0),d0
+		btst	#0,(a2,d0.w)				; has relevant button been pressed?
+		beq.s	.exit						; if not, branch
+		move.b	#1,obStomp_ButtonFlag(a0)	; set active flag
+		move.w	obRespawnAddr(a0),d0		; get address in respawn table
+		beq.s	.update_pos					; if it's zero, don't remember object
+		movea.w	d0,a2						; load address into a2
+		bclr	#7,(a2)						; clear respawn table entry, so object can be loaded again
+; S1 SCE uses 	bset	#0,(a2) here
+
+.update_pos:
+		subi.l	#$10000,obX(a0)				; move left 1px
+		addi.l	#$8000,obY(a0)				; move down 0.5px
+		move.w	obX(a0),obStomp_StartX(a0)
+		cmpi.w	#$980,obX(a0)				; has door reached target position?
+		beq.s	.finish						; if yes, branch
+
+.exit:
+		rts	
+; ===========================================================================
+
+.finish:
+		clr.b	obSubtype(a0)				; change type to 0 (doesn't move)
+		clr.b	obStomp_ButtonFlag(a0)
+		rts	
+; ===========================================================================
