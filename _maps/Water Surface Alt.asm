d8c7a70c27e0adbdbf010e6d523c490cba3a66ae
KoH <cmgaudet2010@gmail.com>
2026-02-12 00:22:57 +0700

    Updated Alt Water Surface Art
    Uses the CPZ art, and proper animation. Alt2 is for using Sonic's greys for water, if you prefer (might keep, might delete, not sure yet.)

--- _maps/Water Surface Alt.asm
+++ _maps/Water Surface Alt.asm
@@ -1,40 +1,57 @@
 ; ---------------------------------------------------------------------------
-; Sprite mappings - alt water surface (from S2's ARZ)
+; Sprite mappings - water surface (alt from Sonic 2's CPZ/HPZ)
 ; ---------------------------------------------------------------------------
 Map_Surf:	mappingsTable
 	mappingsTableEntry.w	.normal1
 	mappingsTableEntry.w	.normal2
+	mappingsTableEntry.w	.normal3
 	mappingsTableEntry.w	.paused1
 	mappingsTableEntry.w	.paused2
+	mappingsTableEntry.w	.paused3
 
 .normal1:	spriteHeader
-	spritePiece	-$60, -4, 4, 2, 0, 0, 0, 0, 0
-	spritePiece	-$20, -4, 4, 2, 0, 0, 0, 0, 0
-	spritePiece	$20, -4, 4, 2, 0, 0, 0, 0, 0
+	spritePiece -$60, -8, 4, 2, 0, 0, 0, 0, 0
+	spritePiece -$20, -8, 4, 2, 0, 0, 0, 0, 0
+	spritePiece $20, -8, 4, 2, 0, 0, 0, 0, 0
 .normal1_End
 
 .normal2:	spriteHeader
-	spritePiece	-$60, -4, 4, 2, 8, 0, 0, 0, 0
-	spritePiece	-$20, -4, 4, 2, 8, 0, 0, 0, 0
-	spritePiece	$20, -4, 4, 2, 8, 0, 0, 0, 0
+	spritePiece -$60, -8, 4, 2, 8, 0, 0, 0, 0
+	spritePiece -$20, -8, 4, 2, 8, 0, 0, 0, 0
+	spritePiece $20, -8, 4, 2, 8, 0, 0, 0, 0
 .normal2_End
 
+.normal3:	spriteHeader
+	spritePiece -$60, -8, 4, 2, $10, 0, 0, 0, 0
+	spritePiece -$20, -8, 4, 2, $10, 0, 0, 0, 0
+	spritePiece $20, -8, 4, 2, $10, 0, 0, 0, 0
+.normal3_End
+
 .paused1:	spriteHeader
-	spritePiece	-$60, -4, 4, 2, 0, 0, 0, 0, 0
-	spritePiece	-$40, -4, 4, 2, 0, 0, 0, 0, 0
-	spritePiece	-$20, -4, 4, 2, 0, 0, 0, 0, 0
-	spritePiece	0, -4, 4, 2, 0, 0, 0, 0, 0
-	spritePiece	$20, -4, 4, 2, 0, 0, 0, 0, 0
-	spritePiece	$40, -4, 4, 2, 0, 0, 0, 0, 0
+	spritePiece -$60, -8, 4, 2, 0, 0, 0, 0, 0
+	spritePiece -$40, -8, 4, 2, 8, 0, 0, 0, 0
+	spritePiece -$20, -8, 4, 2, 0, 0, 0, 0, 0
+	spritePiece 0, -8, 4, 2, 8, 0, 0, 0, 0
+	spritePiece $20, -8, 4, 2, 0, 0, 0, 0, 0
+	spritePiece $40, -8, 4, 2, 8, 0, 0, 0, 0
 .paused1_End
 
 .paused2:	spriteHeader
-	spritePiece	-$60, -4, 4, 2, 8, 0, 0, 0, 0
-	spritePiece	-$40, -4, 4, 2, 8, 0, 0, 0, 0
-	spritePiece	-$20, -4, 4, 2, 8, 0, 0, 0, 0
-	spritePiece	0, -4, 4, 2, 8, 0, 0, 0, 0
-	spritePiece	$20, -4, 4, 2, 8, 0, 0, 0, 0
-	spritePiece	$40, -4, 4, 2, 8, 0, 0, 0, 0
+	spritePiece -$60, -8, 4, 2, 8, 0, 0, 0, 0
+	spritePiece -$40, -8, 4, 2, $10, 0, 0, 0, 0
+	spritePiece -$20, -8, 4, 2, 8, 0, 0, 0, 0
+	spritePiece 0, -8, 4, 2, $10, 0, 0, 0, 0
+	spritePiece $20, -8, 4, 2, 8, 0, 0, 0, 0
+	spritePiece $40, -8, 4, 2, $10, 0, 0, 0, 0
 .paused2_End
+
+.paused3:	spriteHeader
+	spritePiece -$60, -8, 4, 2, $10, 0, 0, 0, 0
+	spritePiece -$40, -8, 4, 2, 8, 0, 0, 0, 0
+	spritePiece -$20, -8, 4, 2, $10, 0, 0, 0, 0
+	spritePiece 0, -8, 4, 2, 8, 0, 0, 0, 0
+	spritePiece $20, -8, 4, 2, $10, 0, 0, 0, 0
+	spritePiece $40, -8, 4, 2, 8, 0, 0, 0, 0
+.paused3_End
 
 	even
