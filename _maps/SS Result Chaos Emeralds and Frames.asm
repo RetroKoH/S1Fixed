e09fe6753556ef9a13d3c2edc6604d40eb50b6e7
KoH <cmgaudet2010@gmail.com>
2026-02-22 13:45:23 +0700

    New Art (RobiWan) and minor fix

--- _maps/SS Result Chaos Emeralds and Frames.asm
+++ _maps/SS Result Chaos Emeralds and Frames.asm
@@ -2,15 +2,16 @@
 ; Sprite mappings - chaos emeralds from	the special stage results screen
 ; New frames added for empty Chaos Emerald slots
 ; ---------------------------------------------------------------------------
+
 Map_SSRC:	mappingsTable
+	mappingsTableEntry.w	SSRC_0
 	mappingsTableEntry.w	SSRC_1
 	mappingsTableEntry.w	SSRC_2
 	mappingsTableEntry.w	SSRC_3
 	mappingsTableEntry.w	SSRC_4
 	mappingsTableEntry.w	SSRC_5
-	mappingsTableEntry.w	SSRC_6
 	if SuperMod
-	mappingsTableEntry.w	SSRC_7
+	mappingsTableEntry.w	SSRC_6
 	endif
 
 ; New frames for missing emeralds
@@ -24,63 +25,70 @@ Map_SSRC:	mappingsTable
 	mappingsTableEntry.w	SSRC_Empty7
 	endif
 
-SSRC_1:	spriteHeader
+SSRC_0:	spriteHeader
 	spritePiece	-8, -8, 2, 2, 4, 0, 0, 1, 0
+SSRC_0_End
+
+SSRC_1:	spriteHeader
+	spritePiece	-8, -8, 2, 2, 0, 0, 0, 0, 0
 SSRC_1_End
 
 SSRC_2:	spriteHeader
-	spritePiece	-8, -8, 2, 2, 0, 0, 0, 0, 0
+	spritePiece	-8, -8, 2, 2, 4, 0, 0, 2, 0
 SSRC_2_End
 
 SSRC_3:	spriteHeader
-	spritePiece	-8, -8, 2, 2, 4, 0, 0, 2, 0
+	spritePiece	-8, -8, 2, 2, 4, 0, 0, 3, 0
 SSRC_3_End
 
 SSRC_4:	spriteHeader
-	spritePiece	-8, -8, 2, 2, 4, 0, 0, 3, 0
+	spritePiece	-8, -8, 2, 2, 8, 0, 0, 1, 0
 SSRC_4_End
 
 SSRC_5:	spriteHeader
-	spritePiece	-8, -8, 2, 2, 8, 0, 0, 1, 0
+	spritePiece	-8, -8, 2, 2, $C, 0, 0, 1, 0
 SSRC_5_End
 
-SSRC_6:	spriteHeader
-	spritePiece	-8, -8, 2, 2, $C, 0, 0, 1, 0
-SSRC_6_End
-
 	if SuperMod
-SSRC_7:	spriteHeader
+SSRC_6:	spriteHeader
 	spritePiece	-8, -8, 2, 2, 0, 0, 0, 2, 0
-SSRC_7_End
+SSRC_6_End
 	endif
 
 SSRC_Empty1:	spriteHeader
 	spritePiece -8, -8, 2, 2, $10, 0, 0, 1, 0
+	spritePiece -8, -8, 2, 2, $20, 0, 0, 1, 0
 SSRC_Empty1_End
 
 SSRC_Empty2:	spriteHeader
 	spritePiece -8, -8, 2, 2, $14, 0, 0, 0, 0
+	spritePiece -8, -8, 2, 2, $20, 0, 0, 1, 0
 SSRC_Empty2_End
 
 SSRC_Empty3:	spriteHeader
-	spritePiece -8, -8, 2, 2, $18, 0, 0, 2, 0
+	spritePiece -8, -8, 2, 2, $10, 0, 0, 2, 0
+	spritePiece -8, -8, 2, 2, $20, 0, 0, 1, 0
 SSRC_Empty3_End
 
 SSRC_Empty4:	spriteHeader
-	spritePiece -8, -8, 2, 2, $1C, 0, 0, 3, 0
+	spritePiece -8, -8, 2, 2, $10, 0, 0, 3, 0
+	spritePiece -8, -8, 2, 2, $20, 0, 0, 1, 0
 SSRC_Empty4_End
 
 SSRC_Empty5:	spriteHeader
+	spritePiece -8, -8, 2, 2, $18, 0, 0, 1, 0
 	spritePiece -8, -8, 2, 2, $20, 0, 0, 1, 0
 SSRC_Empty5_End
 
 SSRC_Empty6:	spriteHeader
-	spritePiece -8, -8, 2, 2, $24, 0, 0, 1, 0
+	spritePiece -8, -8, 2, 2, $1C, 0, 0, 1, 0
+	spritePiece -8, -8, 2, 2, $20, 0, 0, 1, 0
 SSRC_Empty6_End
 
 	if SuperMod
 SSRC_Empty7:	spriteHeader
-	spritePiece -8, -8, 2, 2, $28, 0, 0, 2, 0
+	spritePiece -8, -8, 2, 2, $14, 0, 0, 2, 0
+	spritePiece -8, -8, 2, 2, $20, 0, 0, 1, 0
 SSRC_Empty7_End
 	endif
 
